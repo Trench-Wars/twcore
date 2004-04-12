3fda7fecf2a66979f8345abb0877592861bbffe3
fon <fon@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2004-04-12 14:26:37 +0000

    Fixed the with new final variables for extra event types and rectified the total number
    git-svn-id: http://twcore.org/svn/trunk@46 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/EventRequester.java
+++ twcore/twcore/core/EventRequester.java
@@ -10,67 +10,82 @@ package twcore.core;
  *
  * @author  harvey
  */
-public class EventRequester {
-    static final int TOTAL_NUMBER = 23; 
-    public static int MESSAGE = 0;
-    public static int PLAYER_ENTERED = 1;
-    public static int ARENA_LIST = 2;    
-    public static int PLAYER_POSITION = 3;
-    public static int PLAYER_LEFT = 4;
-    public static int PLAYER_DEATH = 5;
-    public static int PRIZE = 6;
-    public static int SCORE_UPDATE = 7;
-    public static int WEAPON_FIRED = 8;
-    public static int FREQUENCY_CHANGE = 9;
-    public static int FREQUENCY_SHIP_CHANGE = 10;
-    public static int LOGGED_ON = 11;
-    public static int FILE_ARRIVED = 12;
-    public static int ARENA_JOINED = 13;
-    public static int FLAG_VICTORY = 14;
-    public static int FLAG_REWARD = 15;
-    public static int SCORE_RESET = 16;
-    public static int WATCH_DAMAGE = 17;
-    public static int SOCCER_GOAL = 18;
-    public static int BALL_POSITION = 19;
-    public static int FLAG_POSITION = 20;
-    public static int FLAG_DROPPED = 21;
-    public static int FLAG_CLAIMED = 22;
-    
+public class EventRequester
+{
+	//Changes to this are very detremental.  Please be careful
+	//since the recordbot + lots of other classes depends on these numbers
+	//Only change them if you know what you are doing!
+	//Adding new packets requires the total number to be changed as well
+	public static final int TOTAL_NUMBER = 25;
+	public static final int MESSAGE = 0;
+	public static final int PLAYER_ENTERED = 1;
+	public static final int ARENA_LIST = 2;
+	public static final int PLAYER_POSITION = 3;
+	public static final int PLAYER_LEFT = 4;
+	public static final int PLAYER_DEATH = 5;
+	public static final int PRIZE = 6;
+	public static final int SCORE_UPDATE = 7;
+	public static final int WEAPON_FIRED = 8;
+	public static final int FREQUENCY_CHANGE = 9;
+	public static final int FREQUENCY_SHIP_CHANGE = 10;
+	public static final int LOGGED_ON = 11;
+	public static final int FILE_ARRIVED = 12;
+	public static final int ARENA_JOINED = 13;
+	public static final int FLAG_VICTORY = 14;
+	public static final int FLAG_REWARD = 15;
+	public static final int SCORE_RESET = 16;
+	public static final int WATCH_DAMAGE = 17;
+	public static final int SOCCER_GOAL = 18;
+	public static final int BALL_POSITION = 19;
+	public static final int FLAG_POSITION = 20;
+	public static final int FLAG_DROPPED = 21;
+	public static final int FLAG_CLAIMED = 22;
+	public static final int TURF_FLAG_UPDATE = 23;
+	public static final int TURRET_EVENT = 24;
 
-    private boolean[] array;
-    
-    /** Creates a new instance of EventRequester */
-    public EventRequester() {
-        array = new boolean [TOTAL_NUMBER];
-        declineAll();
-    }
-    
-    public void request( int packetType ){
-        array[packetType] = true;
-    }
-    
-    public void requestAll(){
-        for( int i = 0; i < array.length; i++ ){
-            array[i] = true;
-        }  
-    }
-    
-    public void declineAll(){
-        for( int i = 0; i < array.length; i++ ){
-            array[i] = false;
-        }        
-    }
-    
-    public void decline( int packetType ){
-        array[packetType] = false;
-    }
-    
-    public boolean check( int packetType ){
-        return array[packetType];
-    }
-    
-    public void set( int packetType, boolean value ){
-        array[packetType] = value;
-    }
-    
+	private boolean[] array;
+
+	/** Creates a new instance of EventRequester */
+	public EventRequester()
+	{
+		array = new boolean[TOTAL_NUMBER];
+		declineAll();
+	}
+
+	public void request(int packetType)
+	{
+		array[packetType] = true;
+	}
+
+	public void requestAll()
+	{
+		for (int i = 0; i < array.length; i++)
+		{
+			array[i] = true;
+		}
+	}
+
+	public void declineAll()
+	{
+		for (int i = 0; i < array.length; i++)
+		{
+			array[i] = false;
+		}
+	}
+
+	public void decline(int packetType)
+	{
+		array[packetType] = false;
+	}
+
+	public boolean check(int packetType)
+	{
+		return array[packetType];
+	}
+
+	public void set(int packetType, boolean value)
+	{
+		array[packetType] = value;
+	}
+
 }
