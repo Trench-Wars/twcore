57a2fb1f91ee710e2578003c794291f831e97c62
rob <rob@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2004-04-29 10:21:03 +0000

    Moved to solve issues with moving objects between pubbot and pubhub
    git-svn-id: http://twcore.org/svn/trunk@82 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/twcore/misc/pubcommon/PubBotModule.java
@@ -0,0 +1,25 @@
+package twcore.misc.pubcommon;
+
+import twcore.core.*;
+
+public abstract class PubBotModule extends Module
+{
+  private String ipcChannel;
+  private String pubHubName;
+
+  public void initializeModule(String ipcChannel, String pubHubName)
+  {
+    this.ipcChannel = ipcChannel;
+    this.pubHubName = pubHubName;
+  }
+
+  public String getIPCChannel()
+  {
+    return ipcChannel;
+  }
+
+  public String getPubHubName()
+  {
+    return pubHubName;
+  }
+}
