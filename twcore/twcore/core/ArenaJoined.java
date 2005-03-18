2d15ecc4d62ed6591d4383f12d9ac2bc7434284a
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-03-18 02:51:37 +0000

    Core Documentation Project (CDP)
    git-svn-id: http://twcore.org/svn/trunk@352 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/ArenaJoined.java
+++ twcore/twcore/core/ArenaJoined.java
@@ -1,7 +1,20 @@
 package twcore.core;
 
+/**
+ * <font size="+2"><b>ArenaJoined: <font color="blue">S2C 0x02</b></font></font>
+ * <table border="1" cellspacing="0">
+ * <tr> <th>Offset</th><th>Length</th><th>Description</th> </tr>
+ * <tr> <td>0</td> <td>1</td> <td>Type Byte</td> </tr>
+ * </table><br>
+ * This packet lets you know that you can start sending position packets
+ */
 public class ArenaJoined extends SubspaceEvent
 {
+    /**
+     * Creates a new instance of ArenaJoined, this is called by GamePacketInterpreter
+     * when it recieves the packet.
+     * @param array the ByteArray containing the packet data
+     */
     public ArenaJoined(ByteArray array)
     {
         m_byteArray = array;
