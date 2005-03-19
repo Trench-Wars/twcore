2d3110b2869e1e086b4b9a9f883562207c88ae4c
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-03-19 01:13:02 +0000

    Fixed javadoc index page description
    git-svn-id: http://twcore.org/svn/trunk@356 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/ArenaJoined.java
+++ twcore/twcore/core/ArenaJoined.java
@@ -1,11 +1,12 @@
 package twcore.core;
 
 /**
- * <font size="+2"><b>ArenaJoined: <font color="blue">S2C 0x02</b></font></font>
+ * Event called when a bot enters the arena.
+ * <br><font size="+2"><b>ArenaJoined: <font color="blue">S2C 0x02</b></font></font>
  * <table border="1" cellspacing="0">
  * <tr> <th>Offset</th><th>Length</th><th>Description</th> </tr>
  * <tr> <td>0</td> <td>1</td> <td>Type Byte</td> </tr>
- * </table><br>
+ * </table><br></html>
  * This packet lets you know that you can start sending position packets
  */
 public class ArenaJoined extends SubspaceEvent
