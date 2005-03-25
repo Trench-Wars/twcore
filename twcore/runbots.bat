3b2886b28b81e35c703dc1c703a523e1efb1e839
qan <qan@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-03-25 04:21:30 +0000

    Changed to reflect mysql connector update
    git-svn-id: http://twcore.org/svn/trunk@388 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/runbots.bat
+++ twcore/runbots.bat
@@ -1,1 +1,1 @@
-java -cp twcore.jar;twcore/misc/googleapi.jar;twcore/misc/mysql-connector-java-3.0.6-stable-bin.jar twcore.core.Start
+java -cp twcore.jar;twcore/misc/googleapi.jar;twcore/misc/mysql-connector-java-3.1.7-bin.jar twcore.core.Start
