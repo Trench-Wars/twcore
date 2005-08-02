ff1207e90c039927929681b0a8cbc22e63dde74b
qan <qan@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-08-02 20:56:31 +0000

    Fix for position tracking; added Javadocs
    git-svn-id: http://twcore.org/svn/trunk@757 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/Arena.java
+++ twcore/twcore/core/Arena.java
@@ -1,8 +1,21 @@
 /*
  * Arena.java
- *
- * Created on December 12, 2001, 9:10 AM
+ * 
+ * Arena is used to keep track of player information in the Arena a bot is in.  
+ * It is also used to spectate players in order to receive their position packets,
+ * as like any other client, a TWCore bot only receives position packets from
+ * other players when they are close.
+ * 
+ * NOTE ABOUT PLAYER IDS:
+ * "Player ID" refers to the internal packet ID used by the SS protocol rather than
+ * the player's UserID found in ?userid.  IDs are assigned sequentially arena-wide
+ * rather than zone-wide.  In almost all cases, an ID is sent as 2 bytes; however,
+ * short position packets, used to update frequent info on a player, send ID as 1 byte.
+ * This doesn't become a problem until we have >255 people in an arena, after which a
+ * client with a movement prediction algorithm has no real trouble distinguishing between
+ * two players with the same 1-byte ID, but a bot core does.  Fair warning!  -qan
  */
+
 package twcore.core;
 
 import java.util.*;
@@ -27,6 +40,10 @@ public class Arena {
     private int  m_currentTimer = 0;
     private int  m_updateTimer = 0;
     
+    /**
+     * Creates a new instance of an Arena object.
+     *
+     */
     public Arena() {
         
         m_playerList = Collections.synchronizedMap( new HashMap() );
@@ -37,6 +54,13 @@ public class Arena {
         m_tracker = Collections.synchronizedList( new LinkedList() );
     }
     
+    /**
+     * Clears all data.  Used when the bot changes Arenas.  Note that it
+     * is not foolproof, as there may be a delay between when the arena change
+     * is made and when the data is cleared, resulting in unexpected packets
+     * received.  This is particularly common if two arena changes are made in
+     * a very short amount of time.
+     */
     public void clear(){
         
         m_playerList.clear();
@@ -46,20 +70,36 @@ public class Arena {
         m_tracker.clear();
     }
     
+    /**
+     * Gets an Iterator over the players in the arena.
+     * @return Iterator over players in arena
+     */
     public Iterator getPlayerIterator(){
         
         return m_playerList.values().iterator();
     }
     
+    /**
+     * Gets a mapping of IDs to players in the arena.
+     * @return Map (ID -> Player)
+     */
     public Map getPlayerMap() {
     	return m_playerList;
     }
     
+    /**
+     * Gets an Iterator over IDs of players in the arena.
+     * @return Iterator over IDs of players in arena
+     */
     public Iterator getPlayerIDIterator(){
         
         return m_playerList.keySet().iterator();
     }
     
+    /**
+     * Gets an Iterator over players in ships other than 0/spec in the arena.
+     * @return Iterator over players in ships other than 0/spec in arena
+     */    
     public Iterator getPlayingPlayerIterator(){
         LinkedList list = new LinkedList();
         for( Iterator i = getPlayerIterator(); i.hasNext(); ){
@@ -71,6 +111,10 @@ public class Arena {
         return list.iterator();
     }
 
+    /**
+     * Gets an Iterator over IDs of players in ships other than 0/spec in the arena.
+     * @return Iterator over IDs of players in ships other than 0/spec in arena
+     */    
     public Iterator getPlayingIDIterator(){
         LinkedList list = new LinkedList();
         for( Iterator i = getPlayerIterator(); i.hasNext(); ){
@@ -82,6 +126,11 @@ public class Arena {
         return list.iterator();
     }
     
+    /**
+     * Gets an Iterator over IDs of players on a given freq in the arena.
+     * @param freq Frequency of particular interest
+     * @return Iterator over IDs of players on a given freq in arena
+     */    
     public Iterator getFreqIDIterator( int freq ){
         
         Map freqMap = (Map)m_frequencyList.get( new Integer( freq ));        
@@ -92,6 +141,11 @@ public class Arena {
         }
     }
 
+    /**
+     * Gets an Iterator over players on a given freq in the arena.
+     * @param freq Frequency of particular interest
+     * @return Iterator over players on a given freq in arena
+     */    
     public Iterator getFreqPlayerIterator( int freq ){
         
         Map freqMap = (Map)m_playerList.get( new Integer( freq ));
@@ -99,21 +153,53 @@ public class Arena {
         return freqMap.values().iterator();        
     }
     
+    /**
+     * Gets an Iterator over all flags in the arena.
+     * @return Iterator over all flags in arena
+     */
     public Iterator getFlagIDIterator() {
         
         return m_flagIDList.values().iterator();
     }
-    
+
+    /**
+     * Gets a Flag based on a flag ID provided.
+     * @param flagID Flag ID of interest
+     * @return Flag requested
+     */
     public Flag getFlag( int flagID ) {
         
         return (Flag)m_flagIDList.get( new Integer( flagID ) );
     }
     
+    /**
+     * Gets a Player based on player ID provided.
+     * 
+     * NOTE: It's important to check the returned Player object for a null value
+     * or catch the resulting NullPointerException if you make reference to the
+     * object without checking for null.  This is the single most common error
+     * made by new TWCore botmakers.  Don't trust that the playerID provided from
+     * an event packet will correspond to an existing player.  It's possible that
+     * the event will fire simultaneously as the player leaves, resulting in a null
+     * value for the Player object. 
+     * 
+     * @param playerID Player ID of interest
+     * @return Player requested
+     */
     public Player getPlayer( int playerID ){
         
         return (Player)m_playerList.get( new Integer( playerID ) );
     }
     
+    /**
+     * Gets a Player based on player name provided.
+     * 
+     * NOTE: It's important to check the returned Player object for a null value
+     * before using it, or at least catch the possible NullPointerException. 
+     * 
+     * @param playerName Name of the player of interest
+     * @return Player requested
+     */
     public Player getPlayer( String playerName ){
         Player p = (Player)m_playerList.get( m_playerIDList.get( playerName.toLowerCase() ) );
         if( p == null ){
@@ -130,7 +216,13 @@ public class Arena {
             return p;
         }
     }
+
     
+    /**
+     * Gets the name of a player based on ID. 
+     * @param playerID Player ID of interest
+     * @return Player requested
+     */
     public String getPlayerName( int playerID ){
         
         try {
@@ -140,6 +232,11 @@ public class Arena {
         }
     }
     
+    /**
+     * Gets the ID of a player based on name.
+     * @param playerName Player name of interest
+     * @return Player requested
+     */
     public int getPlayerID( String playerName ){
         try {
             return ((Integer)m_playerIDList.get( playerName.toLowerCase() )).intValue();
@@ -148,10 +245,19 @@ public class Arena {
         }
     }
     
+    /**
+     * Gets the size of the list of players (total in arena)
+     * @return number of players in the arena
+     */
     public int size(){
         return m_playerList.size();
     }
     
+    /**
+     * PlayerEntered event processing.  Fires every time a player enters,
+     * and also when the bot enters an arena.
+     * @param message Event object to be processed
+     */
     public void processEvent( PlayerEntered message ){
         int             frequency;
         Map             frequencyList;
@@ -170,8 +276,14 @@ public class Arena {
         }
         
         frequencyList.put( playerID, player );
+        if( message.getShipType() != 0 )
+            addPlayerToTracker( new Integer( message.getPlayerID() ) );            
     }
     
+    /**
+     * ScoreUpdate event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( ScoreUpdate message ){
         Player          player = (Player)m_playerList.get( new Integer( message.getPlayerID() ) );
         
@@ -180,6 +292,10 @@ public class Arena {
         }
     }
     
+    /**
+     * PlayerPosition event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( PlayerPosition message ){
         Player          player = (Player)m_playerList.get( new Integer( message.getPlayerID() ) );
 
@@ -187,15 +303,11 @@ public class Arena {
             player.updatePlayer( message );
         }
     }
-/*
-    public void processEvent( WeaponUpdate message ){
-        Player          player = (Player)m_playerList.get( new Integer( message.getPlayerID() ) );
-
-        if( player != null ){
-            player.updatePlayer( message );
-        }
-    }*/
     
+    /**
+     * FrequencyChange event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FrequencyChange message ){
         int             oldFrequency;
         int             newFrequency;
@@ -223,10 +335,16 @@ public class Arena {
         
         frequencyList.put( new Integer( message.getPlayerID() ), player );
         
-        if( player.getShipType() != 0 ) addPlayerToTracker( new Integer( message.getPlayerID() ) );
-        else removePlayerFromTracker( new Integer( message.getPlayerID() ) );
+        if( player.getShipType() != 0 )
+            addPlayerToTracker( new Integer( message.getPlayerID() ) );
+        else
+            removePlayerFromTracker( new Integer( message.getPlayerID() ) );
     }
     
+    /**
+     * FrequencyShipChange event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FrequencyShipChange message ){
         int             oldFrequency;
         int             newFrequency;
@@ -258,6 +376,10 @@ public class Arena {
         else removePlayerFromTracker( new Integer( message.getPlayerID() ) );
     }
     
+    /**
+     * PlayerLeft event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( PlayerLeft message ){
         int             oldFrequency;
         Map             frequencyList;
@@ -279,6 +401,14 @@ public class Arena {
         removePlayerFromTracker( new Integer( message.getPlayerID() ) );
     }
     
+    /**
+     * PlayerDeath event processing.  We move the player killed to the back
+     * of the queue because we should receive a "free" position packet when
+     * they return to life, regardless of position, and because during the
+     * spawn delay it's not as worthwhile to spectate their position when we
+     * could be spectating living players instead.
+     * @param message Event object to be processed
+     */
     public void processEvent( PlayerDeath message ){
         Player          killer = (Player)m_playerList.get( new Integer( message.getKillerID() ) );
         Player          killee = (Player)m_playerList.get( new Integer( message.getKilleeID() ) );
@@ -294,6 +424,10 @@ public class Arena {
         addPlayerToTracker( new Integer( message.getKilleeID() ) );
     }
     
+    /**
+     * ScoreReset event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( ScoreReset message ){
         if( message.getPlayerID() != -1 ){
             Player          player = (Player)m_playerList.get( new Integer( message.getPlayerID() ) );
@@ -310,6 +444,10 @@ public class Arena {
         
     }
 
+    /**
+     * FlagVictory event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FlagVictory message ){
         Player          player;
         Map             frequencyList;
@@ -325,6 +463,10 @@ public class Arena {
         m_flagIDList.clear();
     }
 
+    /**
+     * FlagReward event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FlagReward message ){
         Player          player;
         Map             frequencyList;
@@ -339,6 +481,10 @@ public class Arena {
         }
     }
     
+    /**
+     * FlagPosition event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FlagPosition message ) {
         Flag flag;
         if( !m_flagIDList.containsKey( new Integer( message.getFlagID() ) ) ) {
@@ -350,6 +496,10 @@ public class Arena {
         flag.processEvent( message );
     }
     
+    /**
+     * FlagClaimed event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FlagClaimed message ) {
         Flag flag;
         if( !m_flagIDList.containsKey( new Integer( message.getFlagID() ) ) ) {
@@ -361,11 +511,15 @@ public class Arena {
         try {            
             flag.processEvent( message, getPlayer( message.getPlayerID() ).getFrequency() );
         } catch (Exception e) {
-            // Now we won't crash King, but will give bad data.
+            // If given playerID returns a null Player, set team to -1 
             flag.processEvent( message, -1 );            
         }
     }
     
+    /**
+     * FlagDropped event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( FlagDropped message ) {
         Iterator it = getFlagIDIterator();
         while( it.hasNext() ) {
@@ -375,6 +529,10 @@ public class Arena {
         }
     }
     
+    /**
+     * TurfFlagUpdate event processing
+     * @param message Event object to be processed
+     */
     public void processEvent( TurfFlagUpdate message ) {
         Flag flag;
         if( !m_flagIDList.containsKey( new Integer( message.getFlagID() ) ) ) {
@@ -387,7 +545,7 @@ public class Arena {
     }
     
      /** Adds a playing player into the tracker Queue
-     * (PlayerEntered, PlayerPosition, FrequencyShipChange, PlayerDeath)
+     * (PlayerEntered, FrequencyShipChange, PlayerDeath)
      * @param playerID - unique ID of player to add to tracking system
      */
     public void addPlayerToTracker( Integer playerID ) {
@@ -397,7 +555,7 @@ public class Arena {
     }
     
     /** Removes a playing player from the tracker Queue
-     * (FrequencyShipChange, PlayerLeft)
+     * (PlayerLeft)
      * @param playerID - unique ID of player to remove from tracking system
      */
     public void removePlayerFromTracker( Integer playerID ) {
@@ -431,9 +589,13 @@ public class Arena {
      */
     public Integer getNextPlayer() {
         
-        if( m_tracker.size() > 0 )
-            return (Integer)m_tracker.get( 0 );
-        else return new Integer( -1 );
+        if( m_tracker.size() > 0 ) {
+            Integer i = (Integer)m_tracker.get( 0 );
+            m_tracker.remove( 0 );
+            m_tracker.add( i );
+            return i;
+        } else
+            return new Integer( -1 );
         
     }
     
