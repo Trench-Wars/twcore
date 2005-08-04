0caa49e72f1b570382e26500470c2461dbc44a06
qan <qan@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-08-04 10:52:57 +0000

    Comments
    git-svn-id: http://twcore.org/svn/trunk@761 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/IPCMessage.java
+++ twcore/twcore/core/IPCMessage.java
@@ -1,11 +1,22 @@
 package twcore.core;
 
+/**
+ * IPCMessage
+ * 
+ * Representing class of messages of the Inter-process Communication protocol of TWCore.
+ * Used to transmit messages between bots.    
+ * 
+ */
 public class IPCMessage
 {
   private String message;
   private String sender;
   private String recipient;
 
+  /**
+   * IPCMessage constructor given only a message.
+   * @param message String of message to create
+   */
   public IPCMessage(String message)
   {
     this.message = message;
@@ -13,6 +24,11 @@ public class IPCMessage
     recipient = null;
   }
 
+  /**
+   * IPCMessage constructor given only a message and recipient.
+   * @param message String of message to create
+   * @param recipient String of recipient to send to
+   */
   public IPCMessage(String message, String recipient)
   {
     this.message = message;
@@ -20,6 +36,12 @@ public class IPCMessage
     sender = null;
   }
 
+  /**
+   * Standard IPCMessage constructor.
+   * @param message String of message to create
+   * @param recipient String of recipient to send to
+   * @param sender String of person sending message
+   */
   public IPCMessage(String message, String recipient, String sender)
   {
     this.message = message;
@@ -27,16 +49,28 @@ public class IPCMessage
     this.sender = sender;
   }
 
+  /**
+   * Return the text of this message.
+   * @return String containing the message text.
+   */
   public String getMessage()
   {
     return message;
   }
 
+  /**
+   * Return the sender of this message.
+   * @return String containing the message sender.
+   */
   public String getSender()
   {
     return sender;
   }
 
+  /**
+   * Return the recipient of this message.
+   * @return String containing the message recipient.
+   */
   public String getRecipient()
   {
     return recipient;
