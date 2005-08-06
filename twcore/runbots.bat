7378a933956170e200f18f4732a7f7c6cc54fda8
lnx <lnx@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-08-06 09:44:08 +0000

    mysql connector update; added JS's aim.jar
    git-svn-id: http://twcore.org/svn/trunk@764 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/runbots.bat
+++ twcore/runbots.bat
@@ -1,3 +1,3 @@
 :Start
-java -Xmx386m -cp twcore.jar;twcore/misc/googleapi.jar;twcore/misc/mysql-connector-java-3.1.7-bin.jar twcore.core.Start
+java -Xmx386m -cp twcore.jar;twcore/misc/googleapi.jar;twcore/misc/aim.jar;twcore/misc/mysql-connector-java-3.1.10-bin.jar twcore.core.Start
 GOTO Start
