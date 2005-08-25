8a441305a7b0e04fe8e17548d8806dea4f28ffed
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-08-25 02:23:11 +0000

    Core Documentation Project (CDP)
    git-svn-id: http://twcore.org/svn/trunk@807 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/PlayerEntered.java
+++ twcore/twcore/core/PlayerEntered.java
@@ -1,25 +1,56 @@
 package twcore.core;
 
+/**
+ * (S2C 0x03) Event fired when a player (or the bot if non-empty) enters the arena. <code><pre>
+ * +------------------------------+
+ * | Offset  Length  Description  |
+ * +--------------------------------+
+ * | 0       1       Type Byte      |
+ * | 1       1       Ship Type      |
+ * | 2       1       Accepts Audio  |
+ * | 3       20      Player Name    |
+ * | 23      20      Squad Name     |
+ * | 43      4       Flag Points    |
+ * | 47      4       Kill Points    |
+ * | 51      2       Player ID      |
+ * | 53      2       Frequency      |
+ * | 55      2       Wins           |
+ * | 57      2       Losses         |
+ * | 59      2       Attachee ID    |
+ * | 61      2       Flags Held     |
+ * | 63      1       Has KOTH Timer |
+ * +--------------------------------+</code></pre>
+ *
+ * Upon entering an arena (or possibly some other instances) several
+ * of these packets (including the Type Byte) are stacked on top
+ * of each other and sent as one packet. Be sure to process this
+ * packet in 64 byte chunks until there are no chunks left.
+ */
 public class PlayerEntered extends SubspaceEvent {
 
     short           m_team;
     short           m_wins;
     short           m_losses;
-    byte            m_hasKOTH;
+    boolean         m_hasKOTH;
     byte            m_shipType;
     String          m_squadName;
     String          m_playerName;
     int             m_flagPoints;
     int             m_killPoints;
     short           m_playerID;
-    byte            m_acceptsAudio;
+    boolean         m_acceptsAudio;
     short           m_flagsCarried;
     short           m_identTurretee;
 
+   /**
+     * Creates a new instance of PlayerEntered, this is called by
+     * GamePacketInterpreter when it receives the packet
+     * @param array the ByteArray containing the packet data
+     */
     public PlayerEntered( ByteArray array ){
-        
+
         m_shipType = (byte)((array.readByte( 1 ) + 1) % 9);
-        m_acceptsAudio = array.readByte( 2 );
+        m_acceptsAudio = array.readByte( 2 ) == 1;
         m_playerName = array.readString( 3, 20 );
         m_squadName = array.readString( 23,20 );
         m_flagPoints = array.readLittleEndianInt( 43 );
@@ -30,69 +61,121 @@ public class PlayerEntered extends SubspaceEvent {
         m_losses = array.readLittleEndianShort( 57 );
         m_identTurretee = array.readLittleEndianShort( 59 );
         m_flagsCarried = array.readLittleEndianShort( 61 );
-        m_hasKOTH = array.readByte( 63 );
+        m_hasKOTH = array.readByte( 63 ) == 1;
     }
 
+    /**
+     * Gets the team of the entering player
+     * @return player's team
+     */
     public short getTeam(){
 
         return m_team;
     }
 
+    /**
+     * Gets how many kills the entering player has
+     * @return player's wins
+     */
     public short getWins(){
 
         return m_wins;
     }
 
+    /**
+     * Gets how many deaths the entering player has
+     * @return player's losses
+     */
     public short getLosses(){
-        
+
         return m_losses;
     }
 
-    public byte getHasKOTH(){
+    /**
+     * Gets whether the entering player is currently in the running for King of the Hill (KOTH)
+     * @return player's KOTH status
+     */
+    public boolean getHasKOTH(){
 
         return m_hasKOTH;
     }
 
+    /**
+     * Gets entering player's ship type
+     * @return player's ship number
+     */
     public byte getShipType(){
 
         return m_shipType;
     }
 
+    /**
+     * Gets the name of the entering player's squad
+     * @return player's squad name
+     */
     public String getSquadName(){
-        
+
         return m_squadName;
     }
 
+    /**
+     * Gets the entering player's name
+     * @return player's name
+     */
     public String getPlayerName(){
 
         return m_playerName;
     }
 
+    /**
+     * Gets the number of points the entering player has from flags
+     * @return player's flag points
+     */
     public int getFlagPoints(){
 
         return m_flagPoints;
     }
 
+    /**
+     * Gets the number of points the entering player has from kills
+     * @return player's kill points
+     */
     public int getKillPoints(){
 
         return m_killPoints;
     }
 
+    /**
+     * Gets the ID of the entering player
+     * @return player's ID
+     */
     public short getPlayerID(){
 
         return m_playerID;
     }
 
-    public byte getAcceptsAudio(){
+    /**
+     * Gets whether the entering player accepts audio (voice) messages
+     * @return if player accepts audio
+     */
+    public boolean getAcceptsAudio(){
 
         return m_acceptsAudio;
     }
 
+    /**
+     * Gets the number of flags carried by the entering player
+     * @return player's flag carry count
+     */
     public short getFlagsCarried(){
 
         return m_flagsCarried;
     }
 
+    /**
+     * Gets the ID of the player the entering player is attached to
+     * @return player's turret's player ID
+     */
     public short getIdentTurretee(){
 
         return m_identTurretee;
