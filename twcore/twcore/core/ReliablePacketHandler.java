78f20685d63a96ceb084eadffea210204f9cab3a
qan <qan@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2005-10-05 18:24:19 +0000

    Javadoc'd
    git-svn-id: http://twcore.org/svn/trunk@846 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/core/ReliablePacketHandler.java
+++ twcore/twcore/core/ReliablePacketHandler.java
@@ -2,17 +2,30 @@ package twcore.core;
 
 import java.util.*;
 
+/**
+ * Handles sending and receipt of reliable packets.  Reliable packets are packets
+ * that require the receiver to return an ACKnowledgement packet.  The ACK packet
+ * contains an ID specified in the head of the reliable packet that lets the sender
+ * know the packet went through okay.  If an ACK is not received, the reliable
+ * packet must be resent.
+ */
 public class ReliablePacketHandler {
 
-    Map                     m_sentPackets;
-    Map                     m_receivedPackets;
-    int                     m_nextOutboundAck;
-    int                     m_expectedReliable;
+    Map                     m_sentPackets;        // (Integer)ACK ID -> ReliablePacket
+    Map                     m_receivedPackets;    // (Integer)ACK ID -> ReliablePacket
+    int                     m_nextOutboundAck;    // ACK ID of next outbound packet 
+    int                     m_expectedReliable;   // ACK ID of the next expected packet
 
-    SSEncryption            m_ssEncryption;
-    GamePacketGenerator     m_packetGenerator;
-    GamePacketInterpreter   m_packetInterpreter;
+    SSEncryption            m_ssEncryption;       // Encryption class
+    GamePacketGenerator     m_packetGenerator;    // Packet generator
+    GamePacketInterpreter   m_packetInterpreter;  // Packet interpreter
 
+    /**
+     * Creates a new instance of ReliablePacketHandler.
+     * @param packetGenerator Packet generator
+     * @param packetInterpreter Packet interpreter
+     * @param ssEncryption Encryption alg
+     */
     public ReliablePacketHandler( GamePacketGenerator packetGenerator, GamePacketInterpreter packetInterpreter, SSEncryption ssEncryption ){
 
         m_nextOutboundAck = 0;
@@ -24,6 +37,11 @@ public class ReliablePacketHandler {
         m_receivedPackets = Collections.synchronizedMap( new HashMap() );
     }
 
+    /**
+     * Constructs and encrypts an outgoing message, and assigns it the next
+     * available ACK ID.
+     * @param message Reliable packet to be handled
+     */
     public void sendReliableMessage( ByteArray message ){
 
         ByteArray           outgoingPacket = new ByteArray( message.size() + 6 );
@@ -39,6 +57,16 @@ public class ReliablePacketHandler {
         m_nextOutboundAck++;
     }
 
+    /**
+     * Handles received packets.  First sends an ACK in response to the packet
+     * with the specified ACK ID, and then checks to see if that same ID
+     * corresponds to the next packet it expects to receive.  If not, the packet
+     * has arrived out of order, and is placed in a queue to be processed after
+     * earlier packets have arrived.  If on the other hand the ID matches the
+     * expected ID, the packet is sent off to be interpreted, as are any other
+     * packets that were received out of order before this one.
+     * @param message Reliable packet to be handled
+     */
     public void handleReliableMessage( ByteArray message ){
 
         ByteArray  subMessage;
@@ -54,9 +82,11 @@ public class ReliablePacketHandler {
             subMessage.addPartialByteArray( message, 0, 6, message.size() - 6 );
             m_packetInterpreter.translateGamePacket( subMessage, true );
 
-            while( (subMessage = (ByteArray)m_receivedPackets.get( new Integer( m_expectedReliable ) )) != null ){
+            subMessage = (ByteArray)m_receivedPackets.remove( new Integer( m_expectedReliable ) );
+            while( subMessage != null ) {
                 m_expectedReliable++;
                 m_packetInterpreter.translateGamePacket( subMessage, true );
+                subMessage = (ByteArray)m_receivedPackets.remove( new Integer( m_expectedReliable ) );
             }
         } else if( receivedAck > m_expectedReliable ){
             subMessage = new ByteArray( message.size() - 6 );
@@ -65,6 +95,12 @@ public class ReliablePacketHandler {
         }
     }
 
+    /**
+     * Handles a received ACK message in response to a packet the bot sent.
+     * When the ACK is received, the reliable packet has gone through without
+     * a problem, and the sent packet is removed from the potential resend list.
+     * @param message ACK message received
+     */
     public void handleAckMessage( ByteArray message ){
         int        receivedAck;
 
@@ -72,6 +108,10 @@ public class ReliablePacketHandler {
         m_sentPackets.remove( new Integer( receivedAck ) );
     }
 
+    /**
+     * Resends all packets that have not been ACKnowledged within the amount of
+     * time specified in the method's internal final RESEND_DELAY. (default 3000ms)
+     */
     public void resendUnackedPacket(){
         Integer         i;
         ByteArray       packet;
@@ -100,27 +140,44 @@ public class ReliablePacketHandler {
         }
     }
 
+    /**
+     * ReliablePacket internal data class.
+     */
     class ReliablePacket {
 
         long            m_timeSent;
         ByteArray       m_packet;
 
+        /**
+         * Creates a new instance of ReliablePacket.
+         * @param packet Packet data
+         */
         ReliablePacket( ByteArray packet ){
 
             m_packet = packet;
             m_timeSent = System.currentTimeMillis();
         }
 
+        /**
+         * @return Time when the packet was sent, in milliseconds since Jan 1, 1970
+         */
         long getTimeSent(){
 
             return m_timeSent;
         }
 
+        /**
+         * @return Contents of the packet
+         */
         ByteArray getPacket(){
 
             return m_packet;
         }
 
+        /**
+         * Set when the packet was sent.
+         * @param newTimeSent Send time in milliseconds since Jan 1, 1970
+         */
         void setTimeSent( long newTimeSent ){
 
             m_timeSent = newTimeSent;
--- twcore/twcore/core/Sender.java
+++ twcore/twcore/core/Sender.java
@@ -1,21 +1,26 @@
-/**
- * Sender.java
- *
- * Created on December 12, 2001, 7:28 PM
- *
- * @author  Jeremy
- */
-
 package twcore.core;
+
 import java.net.*;
 import java.util.*;
 
+/**
+ * A packet sending queue.  Packets are formatted properly from inside
+ * GamePacketGenerator, and then added to Sender's queue to be sent out as
+ * soon as possible.  Packets added as high priority are sent first.
+ * 
+ * @author Jeremy
+ */
 public class Sender extends Thread {
-    private DatagramSocket   m_socket;
-    private List             m_packets;
-    private int              m_packetsSent;
-    private List             m_highPriorityPackets;
-    
+    private DatagramSocket   m_socket;              // Connection to server
+    private List             m_packets;             // Regular send packet queue
+    private int              m_packetsSent;         // # packets sent
+    private List             m_highPriorityPackets; // High priority send packet queue
+
+    /**
+     * Creates a new instance of Sender, a packet sending queue.
+     * @param group ThreadGroup of this Session
+     * @param socket Connection to the outside world
+     */
     public Sender( ThreadGroup group, DatagramSocket socket ){
         super( group, "Sender" );
         m_socket = socket;
@@ -25,25 +30,44 @@ public class Sender extends Thread {
         start();
     }
     
+    /**
+     * @return Total number of packets that have been sent
+     */
     public int getNumPacketsSent(){
         
         return m_packetsSent;
     }
 
+    /**
+     * @return True if the connection to the SS server is still active
+     */
     public boolean isConnected(){
 
         return m_socket.isConnected();
     }
-    
+
+    /**
+     * Initiates sending of a created packet by adding it to the send queue. 
+     * @param packet Packet to send 
+     */
     public void send( DatagramPacket packet ){
         m_packets.add( packet );
     }
     
+    /**
+     * Initiates sending of a high priority packet by adding it to the high
+     * priority send queue. 
+     * @param packet Packet to send 
+     */
     public void highPrioritySend( DatagramPacket packet ){
         
         m_highPriorityPackets.add( packet );
     }
     
+    /**
+     * Checks the queue for packets waiting to be sent, and sends any that are
+     * in waiting.
+     */
     public void run(){
         while( m_socket.isConnected() == true && !interrupted()){
             try{
