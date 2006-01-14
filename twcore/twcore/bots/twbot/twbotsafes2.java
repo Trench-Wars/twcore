2d573c96c62bd89b34f3ecb84d0ed51ae1e33690
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2006-01-14 15:43:26 +0000

    Safes plugin with more options, requested by crazi
    git-svn-id: http://twcore.org/svn/trunk@1028 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/twcore/bots/twbot/twbotsafes2.java
@@ -0,0 +1,172 @@
+package twcore.bots.twbot;
+
+import twcore.core.BotAction;
+import twcore.core.CommandInterpreter;
+import twcore.core.Message;
+import twcore.core.PlayerPosition;
+import twcore.misc.tempset.TempSettingsManager;
+import static twcore.core.OperatorList.ER_LEVEL;
+import static twcore.misc.tempset.SType.*;
+
+/**
+ * Extended Safes Module (Based on twbotsafes by 2dragons)
+ * This was a modification requested by crazi, with the added ability to change
+ * a player's ship and/or frequency when they fly over a safety tile as well
+ * as put them into spectator mode. Unfortunately, due to the way twbot modules
+ * work, none of the settings will be able to be changed and nothing will happen
+ * if a player flies over a safe until the bot operator uses !activate.
+ *
+ * @author D1st0rt
+ * @version 06.01.14
+ */
+public class twbotsafes2 extends TWBotExtension
+{
+	/** The TempSettingsManager used to keep track of the settings */
+	private TempSettingsManager m_tsm;
+
+	/** The CommandInterpreter that passes commands to the TempSettingsManager */
+	private CommandInterpreter m_cmd;
+
+	/** The status of the module's reactions to players flying over safes */
+	private boolean	m_active;
+
+	/** The help message to be sent to bot operators */
+	private final String helpMessage[] =
+	{
+		"+-----------------Extended Safes Module-------------------+",
+		"! !activate - Toggles the module doing anything when a    |",
+		"|             player flies over a safety tile             |",
+		"|                                                         |",
+		"| !set      - Modify actions to take for safety'd players |",
+		"|   |                                                     |",
+		"|   +-SpecPlayer= on/off, Whether to spec player          |",
+		"|   +-SpecMsg   = on/off, Arena message when player spec'd|",
+		"|   +-ChangeShip= on/off, Whether to change player's ship |",
+		"|   +-TargetShip= 1 - 8 , ship to change player to        |",
+		"|   +-ShipChgMsg= on/off, Arena message when ship changed |",
+		"|   +-ChangeFreq= on/off, Whether to change player's freq |",
+		"|   +-TargetFreq= 0-9999, freq to change player to        |",
+		"|   +-FreqChgMsg= on/off, Arena message when freq changed |",
+		"|                                                         |",
+		"| !get      - Get the value for one of the above settings |",
+		"+---------------------------------------------------------+"
+	};
+
+	/**
+	 * Creates a new instance of the Extended Safes Module
+	 */
+	public twbotsafes2()
+	{
+		m_active = false;
+		m_tsm = null;
+		m_cmd = null;
+	}
+
+	/**
+	 * Gets the help message for this module
+	 * @return A string array containing the help information
+	 */
+	public String[] getHelpMessages()
+	{
+		return helpMessage;
+	}
+
+	/**
+	 * Event: Message
+	 * Check and pass to appropriate commands
+	 */
+	public void handleEvent(Message event)
+	{
+		if(m_cmd != null)
+			m_cmd.handleEvent(event);
+		int senderID = event.getPlayerID();
+		String sender = m_botAction.getPlayerName(senderID);
+		String command = event.getMessage().toLowerCase().trim();
+
+		if(event.getMessageType() == Message.PRIVATE_MESSAGE && m_opList.isER(sender))
+			if(command.startsWith("!activate"))
+				c_Activate(sender);
+	}
+
+	/**
+	 * Event: PlayerPosition
+	 * Check to see if player is in a safety zone and respond accordingly
+	 */
+	public void handleEvent( PlayerPosition event )
+	{
+
+		String name = m_botAction.getPlayerName( event.getPlayerID());
+
+		if(m_active && event.isInSafe())
+		{
+			if((Boolean)m_tsm.getSetting("SpecPlayer"))
+			{
+				m_botAction.spec(event.getPlayerID());
+				m_botAction.spec(event.getPlayerID());
+				if((Boolean)m_tsm.getSetting("SpecMsg"))
+					m_botAction.sendArenaMessage(name + " has been specced for going into a safe area.");
+			}
+
+			if((Boolean)m_tsm.getSetting("ChangeShip"))
+			{
+				int ship = (Integer)m_tsm.getSetting("TargetShip");
+				m_botAction.setShip(event.getPlayerID(), ship);
+				if((Boolean)m_tsm.getSetting("ShipChgMsg"))
+					m_botAction.sendArenaMessage(name + " is now in ship "+ ship +" for going into a safe area.");
+			}
+
+			if((Boolean)m_tsm.getSetting("ChangeFreq"))
+			{
+				int freq = (Integer)m_tsm.getSetting("TargetFreq");
+				m_botAction.setFreq(event.getPlayerID(), freq);
+				if((Boolean)m_tsm.getSetting("FreqChgMsg"))
+					m_botAction.sendArenaMessage(name + " is now on freq "+ freq +" for going into a safe area.");
+			}
+
+		}
+	}
+
+	/**
+	 * Command: !activate
+	 * Toggles the module doing anything when a player flies over a safety tile
+	 */
+	private void c_Activate( String sender )
+	{
+		if(m_tsm == null)
+		{
+			m_cmd = new CommandInterpreter(m_botAction);
+			m_tsm = new TempSettingsManager(m_botAction, m_cmd, ER_LEVEL);
+			registerSettings();
+		}
+
+		m_active = !m_active;
+
+		if( m_active ) {
+			m_botAction.sendSmartPrivateMessage( sender, "Reacting to players flying over safety tiles." );
+		} else {
+			m_botAction.sendSmartPrivateMessage( sender, "NOT Reacting to players flying over safety tiles." );
+		}
+	}
+
+	/**
+	 * Establishes all of the settings with the TempSettings Manager
+	 */
+	private void registerSettings()
+	{
+		m_tsm.addSetting(BOOLEAN, "SpecPlayer", "off");
+		m_tsm.addSetting(BOOLEAN, "SpecMsg",    "off");
+
+		m_tsm.addSetting(BOOLEAN, "ChangeShip", "off");
+		m_tsm.addSetting(INT,     "TargetShip", "3");
+		m_tsm.addSetting(BOOLEAN, "ShipChgMsg", "off");
+
+		m_tsm.addSetting(BOOLEAN, "ChangeFreq", "off");
+		m_tsm.addSetting(INT,     "TargetFreq", "1");
+		m_tsm.addSetting(BOOLEAN, "FreqChgMsg", "off");
+
+		m_tsm.restrictSetting("TargetShip", 1, 8);
+		m_tsm.restrictSetting("TargetFreq", 0, 9999);
+	}
+
+	public void cancel(){ }
+}
