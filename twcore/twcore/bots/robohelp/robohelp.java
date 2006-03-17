66679eb4615bb3bb1680023ae80712efb2dbbaed
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2006-03-17 06:12:02 +0000

    Removed debugging statements
    git-svn-id: http://twcore.org/svn/trunk@1061 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/bots/robohelp/robohelp.java
+++ twcore/twcore/bots/robohelp/robohelp.java
@@ -997,10 +997,6 @@ public class robohelp extends SubspaceBot {
     }
 
     public void updateStatRecordsONIT( String name ) {
-        if( !m_botAction.SQLisOperational() ){
-        	m_botAction.sendArenaMessage("SQL Failed.");
-        }
-
         try {
             Calendar thisTime = Calendar.getInstance();
             java.util.Date day = thisTime.getTime();
@@ -1008,14 +1004,11 @@ public class robohelp extends SubspaceBot {
             ResultSet result = m_botAction.SQLQuery(mySQLHost, "SELECT * FROM tblCall WHERE fcUserName = '"+name+"' AND fnType = 0 AND fdDate = '"+time+"'" );
             if(result.next()) {
                 m_botAction.SQLQuery( mySQLHost, "UPDATE tblCall SET fnCount = fnCount + 1 WHERE fcUserName = '"+name+"' AND fnType = 0 AND fdDate = '"+time+"'" );
-				m_botAction.sendArenaMessage("Update called.");
             } else {
 				m_botAction.SQLQuery( mySQLHost, "INSERT INTO tblCall (`fnCallID`, `fcUserName`, `fnCount`, `fnType`, `fdDate`) VALUES ('', '"+name+"', '1', '0', '"+time+"')" );
-				m_botAction.sendArenaMessage("Insert called.");
 		    }
         } catch ( Exception e ) {
 		    System.out.println( "Could not update Stat Records" );
-		    m_botAction.sendArenaMessage("Exception in updateStatRecordsONIT");
 		}
     }
 
