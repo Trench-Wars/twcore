9ec8164e40b8613237051d941057fc9e95950362
d1st0rt <d1st0rt@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2006-06-11 02:23:29 +0000

    Package restructuring requires a new build script
    git-svn-id: http://twcore.org/svn/trunk@1155 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/BuildTWCore.java
+++ twcore/BuildTWCore.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 /**
  * Tool for recursively compiling all or part of TWCore.
- * 
+ *
  * In order for this utility to run properly, you must have your PATH variable
  * set to the Java bin directory where javac is located, or manually set the
  * binDir variable here.
- * 
+ *
  * @author  Stefan / Mythrandir
  */
 public class BuildTWCore {
@@ -17,20 +17,19 @@ public class BuildTWCore {
     String binDir = "";             // Location of Java bin directory
     String bldCmd = "";             // Combination of bin dir and the command being used (automatically set)
     Runtime runtime;                // Runtime process
-    
+
     // Command line flags
-    boolean bbuildCore = false;     // True to build core
-    boolean bbuildAllMisc = false;  // True to build misc
+    boolean bbuildAllCore = false;  // True to build the entire core
     boolean bbuildAllBots = false;  // True to build bots
     boolean bclearAll = false;      // True to remove all class files and twcore.jar after compile
     boolean bclearNBATT = false;    // True to remove all NetBeans project .nbattr files after compile
     LinkedList botList;             // List of specific bots to compile
-    LinkedList miscList;            // List of specific parts of misc to compile
+    LinkedList coreList;            // List of specific parts of misc to compile
     String currentOS;               // Operating system running
-    
+
     long startTime;                 // Time when the compile began
-    
-    
+
+
     /**
      * Creates a new instance of BuildTWCore.
      */
@@ -38,13 +37,13 @@ public class BuildTWCore {
         // get runtime environment
         runtime = Runtime.getRuntime();
         botList = new LinkedList();
-        miscList = new LinkedList();
+        coreList = new LinkedList();
         currentOS = System.getProperty("os.name").toLowerCase();
-        if (currentOS.startsWith("windows")) extraCP = extraCP.replace(':', ';'); 
+        if (currentOS.startsWith("windows")) extraCP = extraCP.replace(':', ';');
         handleArguments(args);
     }
-    
-    
+
+
     /**
      * Handles the arguments supplied on the command line, setting various options
      * for the build.
@@ -54,16 +53,14 @@ public class BuildTWCore {
         String s;
         for (int i=0; i<args.length; i++) {
             s = args[i];
-            
+
             if (s.equals("all")) {
-                bbuildCore = true;
-                bbuildAllMisc = true;
+                bbuildAllCore = true;
                 bbuildAllBots = true;
             };
-            
-            if (s.equals("core")) bbuildCore = true;
-            else if (s.equals("misc")) bbuildAllMisc = true;
-            else if (s.startsWith("misc")) miscList.add("twcore/" + s);
+
+            if (s.equals("core")) bbuildAllCore = true;
+            else if (s.startsWith("core")) coreList.add("twcore/" + s);
             else if (s.equals("bots")) bbuildAllBots = true;
             else if (s.equals("clear")) bclearAll = true;
             else if (s.equals("clearnb")) bclearNBATT = true;
@@ -71,31 +68,29 @@ public class BuildTWCore {
                 System.out.println("Usage:");
                 System.out.println("  build all    (builds the entire bot)");
                 System.out.println("  build core   (builds the core directory)");
-                System.out.println("  build misc   (builds the entire misc directory)");
-                System.out.println("  build misc/<dirname>   (builds only that directory within misc, not recursive)");
+                System.out.println("  build core/<dirname>   (builds only that directory within core)");
                 System.out.println("  build bots   (builds the entire bots directory)");
                 System.out.println("  build <botname> <botname> <botname>   (builds only the specified bots)");
                 System.out.println("  build clear  (clears all jar and class files after compile)");
                 System.out.println("  build clear clearnb (clears all jar, class and netbeans proj files after compile)");
                 System.out.println();
                 System.out.println("Examples:");
-                System.out.println("build misc matchbot pubbot");
-                System.out.println("build core misc/      (will only build files in misc/)");
-                System.out.println("build core misc/database");
+                System.out.println("build matchbot pubbot");
+                System.out.println("build core");
+                System.out.println("build core/stats core/sql");
 
             } else botList.add(s);
         }
-        
+
         if (args.length == 0) {
-            bbuildCore = true;
-            bbuildAllMisc = true;
+            bbuildAllCore = true;
             bbuildAllBots = true;
         }
     }
-    
-    
+
+
     /**
-     * Executes a new external process and records any output returned from it. 
+     * Executes a new external process and records any output returned from it.
      * @param s Command and arguments to execute
      * @throws Exception
      */
@@ -109,10 +104,10 @@ public class BuildTWCore {
         p.waitFor();
     }
 
-    
+
     /**
      * Executes a new external process and records any output returned from it.
-     * Wrapper for fullExec(String[]). 
+     * Wrapper for fullExec(String[]).
      * @param s Command and arguments to execute
      * @throws Exception
      */
@@ -120,7 +115,7 @@ public class BuildTWCore {
         fullExec(new String[] {s});
     }
 
-    
+
     /**
      * Creates a file list using the contents of the provided directory.
      * @param dir Directory to list
@@ -132,21 +127,31 @@ public class BuildTWCore {
         boolean isnotempty = false;
         if (fList.exists()) fList.delete();
         fList.createNewFile();
-        
+
         PrintWriter out = new PrintWriter( new BufferedWriter( new FileWriter(fList)));
-        File[] coreList = dir.listFiles();
-        
-        for (int i=0;i<coreList.length;i++)
-            if (coreList[i].getName().endsWith(".java")) {
-                out.println(coreList[i].getPath());
+        if(dir.isDirectory())
+        {
+	        File[] coreList = dir.listFiles();
+
+	        for (int i=0;i<coreList.length;i++)
+	            if (coreList[i].getName().endsWith(".java")) {
+	                out.println(coreList[i].getPath());
+	                isnotempty = true;
+	            }
+        }
+        else
+        {
+        	if (dir.getName().endsWith(".java"))
+        	{
+        		out.println(dir.getPath());
                 isnotempty = true;
-            }
-        
+        	}
+        }
+
         out.close();
-        
+
         return isnotempty;
     }
-    
 
     /**
      * Recursively deletes all files specified in the filelist and their children,
@@ -157,18 +162,18 @@ public class BuildTWCore {
         if (f.isDirectory()) {
             File[] flist = f.listFiles();
             for (int i=0; i<flist.length; i++) {
-                if (flist[i].isDirectory()) 
+                if (flist[i].isDirectory())
                     recursiveDelete(flist[i]);
                 else
                     if (flist[i].isFile()) {
                         flist[i].delete();
                     }
-                    
+
             }
             f.delete();
         }
     }
-    
+
 
     /**
      * Recursively deletes class files (and NetBeans project .nbattrs files if specified).
@@ -178,18 +183,18 @@ public class BuildTWCore {
         if (f.isDirectory()) {
             File[] flist = f.listFiles();
             for (int i=0; i<flist.length; i++) {
-                if (flist[i].isDirectory()) 
+                if (flist[i].isDirectory())
                     recursiveDeleteClass(flist[i]);
                 else
-                    if (flist[i].isFile()) 
-                        if (flist[i].getName().endsWith(".class") && !flist[i].getName().equalsIgnoreCase("BuildTWCore.class")) 
+                    if (flist[i].isFile())
+                        if (flist[i].getName().endsWith(".class") && !flist[i].getName().equalsIgnoreCase("BuildTWCore.class"))
                             flist[i].delete();
                         else if (flist[i].getName().equals(".nbattrs") && bclearNBATT) flist[i].delete();
-                    
+
             }
         }
     }
-            
+
 
     /**
      * Recursively compiles the specified directory and all child directories.
@@ -204,31 +209,66 @@ public class BuildTWCore {
             try {
                 System.out.println("Building " + f.getPath());
                 boolean hasContent = createFileList(f, new File("flist.txt"));
-                if (hasContent) {               	
+                if (hasContent) {
                     bldCmd = binDir + "javac";
-                    
+
                     if (!bot)
                         fullExec(new String[] {bldCmd, "-classpath", "twcore.jar" + extraCP, "-sourcepath", f.getPath(), "-d", "temp", "@flist.txt"});
                     else
                         fullExec(new String[] {bldCmd, "-classpath", "twcore.jar" + extraCP, "-sourcepath", f.getPath(), "@flist.txt"});
                 }
             } catch (Exception e) { System.out.println("error... " + e.getMessage()); }
-            
-            
+
+
             File[] flist = f.listFiles();
-            
+
             if (dig)
                 for (int i=0; i<flist.length; i++)
                     if (flist[i].isDirectory()) {
 						recursiveCompile(flist[i], true, bot);
                     }
         }
-    }    
-    
-    
+    }
+
     /**
-     * Compiles all files in the core directory. 
+     * Recursively compiles the specified directory and all child directories.
+     * @param f Directory to begin recursion from
+     * @param dig True if recursion should continue
+     * @param bot True if the directory being compiled belongs to a bot
+     * @param sourcepath
      */
+    public void recursiveCompile(File f, boolean dig, boolean bot, String extrapath) {
+        if (f.isDirectory() && !f.getName().endsWith("CVS") ) {
+
+            // Compile all the files located in here into temp
+            try {
+                System.out.println("Building " + f.getPath());
+                boolean hasContent = createFileList(f, new File("flist.txt"));
+                if (hasContent) {
+                    bldCmd = binDir + "javac";
+
+                    if (!bot)
+                        fullExec(new String[] {bldCmd, "-classpath", "\""+extrapath+"\"", "-d", "temp", "@flist.txt"});
+                    else
+                        fullExec(new String[] {bldCmd, "-classpath", "\""+extrapath+"\"", "@flist.txt"});
+                }
+            } catch (Exception e) { System.out.println("error... " + e.getMessage()); }
+
+
+            File[] flist = f.listFiles();
+
+            if (dig)
+                for (int i=0; i<flist.length; i++)
+                    if (flist[i].isDirectory()) {
+						recursiveCompile(flist[i], true, bot, extrapath);
+                    }
+        }
+    }
+
+
+    /**
+     * Compiles all files in the core directory.
+     *
     public void buildCore() {
         System.out.println("Building core...");
         try {
@@ -241,39 +281,42 @@ public class BuildTWCore {
             createFileList(new File("twcore/core"), new File("flist.txt"));
             bldCmd = binDir + "javac";
             fullExec(new String[] { bldCmd, "-sourcepath", "core", "-d", "temp", "@flist.txt"});
-            
+
             // Create the .jar (not as messy as many class files)
                 bldCmd = binDir + "jar";
             fullExec(new String[] { bldCmd, "cf", "twcore.jar", "-C", "temp", "."});
-            
+
             recursiveDelete(tempDir);
-            
+
         } catch (Exception e) {
             System.out.println("Couldn't build core: " + e.getMessage());
             e.printStackTrace();
         }
         System.out.println("Finished building core");
         System.out.println();
-    }
+    }*/
 
 
     /**
      * Compiles all files in the misc directory, and recursively any in child directories.
      */
-    public void buildMisc() {
+    public void buildCore() {
         try {
             boolean doneSomething = false;
-            
+
             // Create a temporary directory for storage
             File tempDir = new File("temp");
             tempDir.mkdir();
+            File current = new File("");
+            String classpath = current.getAbsolutePath();
 
             // Compile
-            if (bbuildAllMisc) {
-                recursiveCompile(new File("twcore/misc"), true, false);
+            if (bbuildAllCore) {
+            	//fullExec(new String[] { bldCmd, "-sourcepath", "@sourcepath.txt", "-d", "temp", "@flist.txt"});
+                recursiveCompile(new File("twcore/core"), true, false, classpath);
                 doneSomething = true;
             } else {
-                ListIterator i = miscList.listIterator();
+                ListIterator i = coreList.listIterator();
                 String s;
                 while (i.hasNext()) {
                     s = (String)i.next();
@@ -283,79 +326,93 @@ public class BuildTWCore {
                 }
             }
 
-            // Update the .jar with the misc additions
+            // Update the .jar with the additions
             if (doneSomething) {
                 	bldCmd = binDir + "jar";
-                fullExec(new String[] {bldCmd, "uf", "twcore.jar", "-C", "temp", "."});
+                fullExec(new String[] {bldCmd, "cf", "twcore.jar", "-C", "temp", "."});
             }
             recursiveDelete(tempDir);
-            
+
+            if(doneSomething)
+            	System.out.println("Finished building core");
+
         } catch (Exception e) {
-            System.out.println("Couldn't build misc: " + e.getMessage());
+            System.out.println("Couldn't build core: " + e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("Finished building misc");
+
         System.out.println();
     }
 
-    
+
     /**
-     * Recursively compiles all bots in the bots directory. 
+     * Recursively compiles all bots in the bots directory.
      */
-    public void buildBots() { 
-        File[] botDirs = new File("twcore/bots").listFiles();
+    public void buildBots() {
+
+    	if(bbuildAllBots)
+    		recursiveCompile(new File("twcore/bots"), true, true, new File("").getAbsolutePath());
+    	else if(botList.size() > 0)
+    	{
+    		for (int i=0; i < botList.size(); i++)
+    		{
+    			recursiveCompile(new File("twcore/bots/"+botList.get(i)), true, true, new File("").getAbsolutePath());
+    		}
+    	}
+        /*File[] botDirs = new File("twcore/bots").listFiles();
         boolean hasContent;
-        
+
         for (int i=0; i < botDirs.length; i++) {
             if ((bbuildAllBots) || (botList.contains(botDirs[i].getName()))) {
-                try {                    
+                try {
                     hasContent = createFileList(botDirs[i], new File("flist.txt"));
                     // Because the recursive compile handles it, no need to compile here
 					if (hasContent)
-                        recursiveCompile(botDirs[i], true, true);
+                        recursiveCompile(botDirs[i], true, true, new File("").getAbsolutePath());
                 } catch (Exception e) {
                     System.out.println("Error.... " + e.getMessage());
+                    e.printStackTrace();
                 }
             }
-            
-        }
+
+        }*/
 
     }
-    
-    
+
+
     /**
-     * Deletes twcore.jar and all class files. 
+     * Deletes twcore.jar and all class files.
      */
     public void clearAll() {
         try {
             System.out.println("Deleting twcore.jar");
             File twcoreJar = new File("twcore.jar");
             if (twcoreJar.exists()) twcoreJar.delete();
-                        
+
             System.out.println("Deleting *.class in /");
             recursiveDeleteClass(new File("."));
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-        
+
         System.out.println();
         System.out.println("twcore.jar and all .class cleared.");
     }
-    
+
 
     /**
-     * Begins the compiling process. 
+     * Begins the compiling process.
      */
     public void gogogo() {
         startTime = System.currentTimeMillis();
-        if (bbuildCore) buildCore();
-        buildMisc();
+        buildCore();
+        //buildMisc();
         buildBots();
         if (bclearAll) clearAll();
         System.out.println();
         System.out.println("Finished. (" + (System.currentTimeMillis()-startTime) + " ms)");
-        
+
         // clear temp file
         File tmpFile = new File ("flist.txt");
         if (tmpFile.exists()) tmpFile.delete();
@@ -370,6 +427,6 @@ public class BuildTWCore {
         BuildTWCore b = new BuildTWCore(args);
         b.gogogo();
     }
-    
+
 }
 
