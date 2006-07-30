6496f320ce8a737eef4e9e77e075224a44063463
jacen <jacen@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2006-07-30 01:24:14 +0000

    testing something
    git-svn-id: http://twcore.org/svn/trunk@1225 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/twcore/bots/twbot/twbotikrit.java
@@ -0,0 +1,46 @@
+package twcore.bots.twbot;
+
+import twcore.core.*;
+import twcore.core.game.Player;
+import twcore.core.events.PlayerDeath;
+import twcore.core.events.ScoreUpdate;
+import twcore.core.events.PlayerEntered;
+import twcore.core.events.ScoreReset;
+import twcore.bots.*;
+
+public class twbotikrit extends TWBotExtension {
+	
+	public twbotikrit() {
+		
+	}
+	
+	public void handleEvent(PlayerDeath event) {
+		Player killer = m_botAction.getPlayer(event.getPlayerID());
+		if(killer != null) {
+			m_botAction.sendPublicMessage("PlayerDeath event: " + killer.getScore());
+		}
+	}
+	
+	public void handleEvent(ScoreUpdate event) {
+		Player p = m_botAction.getPlayer(event.getPlayerID());
+		m_botAction.sendPublicMessage("ScoreUpdate event: " + p.getScore());
+	}
+	
+	public void handleEvent(PlayerEntered event) {
+		Player p = m_botAction.getPlayer(event.getPlayerID());
+		m_botAction.sendPublicMessage("PlayerEntered event: " + p.getScore());
+	}
+	
+	public void handleEvent(ScoreReset event) {
+		Player p = m_botAction.getPlayer(event.getPlayerID());
+		m_botAction.sendPublicMessage("ScoreReset event: " + p.getScore() + " -" + p.getPlayerName());
+	}
+	
+	public String[] getHelpMessages() {
+		String[] str = { "" };
+		return str;
+	}
+	
+	public void cancel() {
+	}
+}
