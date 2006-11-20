94063faec264bb575e396864afcd7baebd70dd47
jacen <jacen@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2006-11-20 16:02:49 +0000

    Better response if a command fails (in the catch RuntimeException block). Still dirty code though
    git-svn-id: http://twcore.org/svn/trunk@1291 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/bots/multibot/multibot.java
+++ twcore/twcore/bots/multibot/multibot.java
@@ -111,7 +111,8 @@ public class multibot extends SubspaceBot
     }
     catch(RuntimeException e)
     {
-      m_botAction.sendSmartPrivateMessage(sender, "Runtime exception encountered; please notify a member of the coding staff if you believe you have reached this recording in error." );
+      m_botAction.sendSmartPrivateMessage(sender, "Runtime exception encountered; " + e.getMessage());
+      m_botAction.sendSmartPrivateMessage(sender, "If you don't recognize the reason, please notify a member of the coding staff." );
     }
   }
 
