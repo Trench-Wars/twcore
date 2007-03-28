69c7cfef73615becb12ebea7c5a0984ee7fbfd56
dugwyler <dugwyler@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-03-28 13:41:49 +0000

    Cleaned up JavaDocs, and made them more explicit.
    git-svn-id: http://twcore.org/svn/trunk@1417 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/basicbot/basicbot.java
+++ twcore/src/twcore/bots/basicbot/basicbot.java
@@ -1,12 +1,3 @@
-/*
- * ultrabot.java
- *
- * Created on March 17, 2004, 11:45 AM
- *
- * This is the most simple form of a bot. You can use this template whenever
- * you create a new bot
- */
-
 package twcore.bots.basicbot;
 
 import twcore.core.*;
@@ -15,16 +6,21 @@ import twcore.core.events.LoggedOn;
 import twcore.core.events.Message;
 
 /**
+ * This is a barebones template bot for use when creating a new bot.  With slight
+ * modification, it can suit just about any purpose.  Note that if you are
+ * thinking of creating a typical eventbot, you should strongly consider creating
+ * a MultiBot module instead, which is more simple.
  *
  * @author  Stefan / Mythrandir
  */
 public class basicbot extends SubspaceBot {
 
-    //
-    private BotSettings m_botSettings;
+    private BotSettings m_botSettings;          // Stores settings for your bot as found in the .cfg file.
+                                                // In this case, it would be settings from basicbot.cfg
 
-
-    /** Creates a new instance of ultrabot */
+    /**
+     * Creates a new instance of your bot.
+     */
     public basicbot(BotAction botAction) {
         super(botAction);
         requestEvents();
@@ -34,7 +30,10 @@ public class basicbot extends SubspaceBot {
     }
 
 
-    /** Request events that this bot requires to receive.  */
+    /**
+     * This method requests event information from any events your bot wishes
+     * to "know" about; if left commented your bot will simply ignore them.
+     */
     public void requestEvents() {
         EventRequester req = m_botAction.getEventRequester();
         req.request(EventRequester.MESSAGE);
@@ -63,18 +62,20 @@ public class basicbot extends SubspaceBot {
     }
 
 
-    /** write an event handler for each requested packet */
+    /**
+     * You must write an event handler for each requested event/packet.
+     * This is an example of how you can handle a message event.
+     */
     public void handleEvent(Message event) {
-        // retreive name. If the message is remote then event.getMessager() returns null, and event.getPlayerID returns a value
-        // if the message is from the same arena, event.getMessager returns a string, and event.getPlayerID will return 0
+        // Retreive name. If the message is remote, then event.getMessager() returns null, and event.getPlayerID returns a value.
+        // If the message is from the same arena, event.getMessager() returns a string, and event.getPlayerID will return 0.
         String name = event.getMessager() != null ? event.getMessager() : m_botAction.getPlayerName(event.getPlayerID());
         if (name == null) name = "-anonymous-";
 
         m_botAction.sendPublicMessage(  "I received a Message event type ("+ event.getMessageType()+") from " + name +
                                         " containing the following text: " + event.getMessage());
 
-
-        /* most important command: !die */
+        // Default implemented command: !die
         if (event.getMessageType() == Message.PRIVATE_MESSAGE && event.getMessage().equalsIgnoreCase("!die")) {
             m_botAction.sendPublicMessage(name + " commanded me to die. Disconnecting...");
             try { Thread.sleep(50); } catch (Exception e) {};
@@ -84,20 +85,27 @@ public class basicbot extends SubspaceBot {
     }
 
 
-    /* when the bot logs on, you have to manually send it to an arena */
+    /**
+     * The LoggedOn event is fired when the bot has logged on to the system, but
+     * has not yet entered an arena.  A normal SS client would automatically join
+     * a pub; since we do things manually we must tell the bot to join an arena
+     * after it has successfully logged in.  Usually the arena is stored in the
+     * settings file of the bot, which shares the name of the source and class 
+     * files, but has a cfg extension.  
+     */
     public void handleEvent(LoggedOn event) {
         m_botAction.joinArena(m_botSettings.getString("arena"));
     }
 
 
-    /* set ReliableKills 1 (*relkills 1) to make sure your bot receives every packet */
+    /**
+     * Set ReliableKills 1 (*relkills 1) to make sure your bot receives every packet.
+     * If you don't set reliable kills to 1 (which ensures that every death packet
+     * of players 1 bounty and higher [all] will be sent to you), your bot may not
+     * get a PlayerDeath event for every player that dies.
+     */
     public void handleEvent(ArenaJoined event) {
         m_botAction.setReliableKills(1);
     }
 
-
-
-
 }
-
-
