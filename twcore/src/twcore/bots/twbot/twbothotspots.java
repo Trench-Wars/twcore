8577131c08635357539aef751a99da159d0b812a
dugwyler <dugwyler@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-05-24 23:21:01 +0000

    Update to hotspots module: allows a spot to prize a player after warping them.  Also allows clearing of all spots.
    I held off on this update quite a while because someone was making this module DB-accessible ... but, enough time has passed that I think committing this is still keeping with good manners. :P
    
    git-svn-id: http://twcore.org/svn/trunk@1506 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/twbot/twbothotspots.java
+++ twcore/src/twcore/bots/twbot/twbothotspots.java
@@ -1,6 +1,7 @@
 package twcore.bots.twbot;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TimerTask;
 import java.util.Vector;
@@ -16,195 +17,245 @@ import twcore.core.game.Player;
 
 public class twbothotspots extends TWBotExtension {
 
-	HashMap hotSpots;
+    HashSet <HotSpot>hotSpots;
 
-	HashMap playerList;
-	Vector  queueList;
-	String watch = "";
+    HashMap <String,Integer>playerList;
+    Vector  <String>queueList;
+    String watch = "";
 
-	final int SWITCH_TIME = 2000;
+    final int SWITCH_TIME = 2000;
 
-	public twbothotspots() {
+    public twbothotspots() {
 
-		hotSpots = new HashMap();
+        hotSpots = new HashSet<HotSpot>();
 
-		playerList = new HashMap();
-		queueList = new Vector();
-	}
+        playerList = new HashMap<String,Integer>();
+        queueList = new Vector<String>();
+    }
 
-	public void handleEvent( Message event ){
+    public void handleEvent( Message event ){
 
         String message = event.getMessage();
 
         if( event.getMessageType() == Message.PRIVATE_MESSAGE ){
             String name = m_botAction.getPlayerName( event.getPlayerID() );
             if( m_opList.isER( name ))
-            	handleCommand( name, message );
+                handleCommand( name, message );
         }
-	}
+    }
 
     public void handleCommand( String name, String message ) {
 
-    	if( message.toLowerCase().startsWith( "!addspot " ) ) {
-    		do_addHotSpot( name, message.substring( 9, message.length() ) );
-    	} else if( message.toLowerCase().startsWith( "!watch" ) ) {
-	    	TimerTask change = new TimerTask() {
-				public void run() {
-					try {
-						watch = (String)queueList.elementAt(0);
-						queueList.removeElementAt(0);
-						queueList.addElement( watch );
-					} catch (Exception e) {}
-					m_botAction.spectatePlayer( watch );
-				}
-			};
-			m_botAction.scheduleTaskAtFixedRate( change, 2000, SWITCH_TIME );
-		}
+        if( message.toLowerCase().startsWith( "!addspot " ) ) {
+            do_addHotSpot( name, message.substring( 9, message.length() ) );
+        } else if( message.toLowerCase().startsWith( "!prizespot " ) ) {
+            do_addPrizeHotSpot( name, message.substring( 11, message.length() ) );
+        } else if( message.toLowerCase().startsWith( "!clearspots" ) ) {
+            do_clearHotSpots( name );
+        } else if( message.toLowerCase().startsWith( "!watch" ) ) {
+            TimerTask change = new TimerTask() {
+                public void run() {
+                    try {
+                        watch = (String)queueList.elementAt(0);
+                        queueList.removeElementAt(0);
+                        queueList.addElement( watch );
+                    } catch (Exception e) {}
+                    m_botAction.spectatePlayer( watch );
+                }
+            };
+            m_botAction.scheduleTaskAtFixedRate( change, 2000, SWITCH_TIME );
+        }
     }
 
-	public void handleEvent( PlayerEntered event ) {
+    public void handleEvent( PlayerEntered event ) {
 
-		Player p = m_botAction.getPlayer( event.getPlayerID() );
-		String name = m_botAction.getPlayerName( event.getPlayerID() );
+        Player p = m_botAction.getPlayer( event.getPlayerID() );
+        String name = m_botAction.getPlayerName( event.getPlayerID() );
 
-		if( p.getShipType() == 0 ) return;
-		if( !playerList.containsKey( name ) ) {
-			playerList.put( name, getTime() );
-			queueList.addElement( name );
-		}
-	}
+        if( p.getShipType() == 0 ) return;
+        if( !playerList.containsKey( name ) ) {
+            playerList.put( name, getTime() );
+            queueList.addElement( name );
+        }
+    }
 
-	public void handleEvent( PlayerLeft event ) {
+    public void handleEvent( PlayerLeft event ) {
 
-		String name = m_botAction.getPlayerName( event.getPlayerID() );
+        String name = m_botAction.getPlayerName( event.getPlayerID() );
 
-		if( playerList.containsKey( name ) )
-			playerList.remove( name );
-	}
+        if( playerList.containsKey( name ) )
+            playerList.remove( name );
+    }
 
-	public void handleEvent( FrequencyChange event ) {
+    public void handleEvent( FrequencyChange event ) {
 
-		Player p = m_botAction.getPlayer( event.getPlayerID() );
-		String name = m_botAction.getPlayerName( event.getPlayerID() );
+        Player p = m_botAction.getPlayer( event.getPlayerID() );
+        String name = m_botAction.getPlayerName( event.getPlayerID() );
 
-		if( p.getShipType() == 0 ) return;
-		if( !playerList.containsKey( name ) ) {
-			playerList.put( name, getTime() );
-			queueList.addElement( name );
-		}
-	}
+        if( p.getShipType() == 0 ) return;
+        if( !playerList.containsKey( name ) ) {
+            playerList.put( name, getTime() );
+            queueList.addElement( name );
+        }
+    }
 
-	public void handleEvent( FrequencyShipChange event ) {
+    public void handleEvent( FrequencyShipChange event ) {
 
-		Player p = m_botAction.getPlayer( event.getPlayerID() );
-		String name = m_botAction.getPlayerName( event.getPlayerID() );
+        Player p = m_botAction.getPlayer( event.getPlayerID() );
+        String name = m_botAction.getPlayerName( event.getPlayerID() );
 
-		if( p.getShipType() == 0 ) {
-			if( playerList.containsKey( name ) )
-				playerList.remove( name );
-		} else {
-			if( !playerList.containsKey( name ) ) {
-				playerList.put( name, getTime() );
-				queueList.addElement( name );
-			}
-		}
-	}
+        if( p.getShipType() == 0 ) {
+            if( playerList.containsKey( name ) )
+                playerList.remove( name );
+        } else {
+            if( !playerList.containsKey( name ) ) {
+                playerList.put( name, getTime() );
+                queueList.addElement( name );
+            }
+        }
+    }
 
-	public void handleEvent( PlayerPosition event ) {
+    public void handleEvent( PlayerPosition event ) {
 
-		Player p = m_botAction.getPlayer( event.getPlayerID() );
-		String name = m_botAction.getPlayerName( event.getPlayerID() );
+        Player p = m_botAction.getPlayer( event.getPlayerID() );
+        String name = m_botAction.getPlayerName( event.getPlayerID() );
 
-		if( !playerList.containsKey( name ) ) {
-			playerList.put( name, getTime() );
-			queueList.addElement( name );
-		}
+        if( !playerList.containsKey( name ) ) {
+            playerList.put( name, getTime() );
+            queueList.addElement( name );
+        }
 
-		//Need to reset players it sees
-		if( name != null)
-		    resetPlayerQueue( name );
+        //Need to reset players it sees
+        if( name != null)
+            resetPlayerQueue( name );
 
-		Iterator it = hotSpots.keySet().iterator();
-		while( it.hasNext() ) {
-			String key = (String)it.next();
-			HotSpot spot = (HotSpot)hotSpots.get( key );
-			if( spot != null )
+        Iterator it = hotSpots.iterator();
+        while( it.hasNext() ) {
+            HotSpot spot = (HotSpot)it.next();
+            if( spot != null )
                 if( p != null )
-                    if( spot.inside( p.getXLocation(), p.getYLocation() ) )
+                    if( spot.inside( p.getXLocation(), p.getYLocation() ) ) {
                         m_botAction.warpTo( name, spot.getX(), spot.getY() );
-		}
-	}
+                        if( spot.getPrize() != -1 )
+                            m_botAction.specificPrize( name, spot.getPrize() );
+                    }
+        }
+    }
 
-	public Integer getTime() {
-		return new Integer( (int)(System.currentTimeMillis()/100) );
-	}
+    public Integer getTime() {
+        return new Integer( (int)(System.currentTimeMillis()/100) );
+    }
 
-	public void resetPlayerQueue( String name ) {
-		for( int i = 0; i < queueList.size(); i++ ) {
-			String thisName = (String)queueList.elementAt( i );
-			if( thisName != null && thisName.equals( name ) ) {
-				queueList.removeElementAt( i );
-				queueList.addElement( name );
-				i = queueList.size();
-			}
-		}
-	}
+    public void resetPlayerQueue( String name ) {
+        for( int i = 0; i < queueList.size(); i++ ) {
+            String thisName = (String)queueList.elementAt( i );
+            if( thisName != null && thisName.equals( name ) ) {
+                queueList.removeElementAt( i );
+                queueList.addElement( name );
+                i = queueList.size();
+            }
+        }
+    }
 
-	public void do_addHotSpot( String name, String message ) {
+    public void do_addHotSpot( String name, String message ) {
 
-		String pieces[] = message.split( " " );
-		if( pieces.length != 5 ) return;
+        String pieces[] = message.split( " " );
+        if( pieces.length != 5 ) return;
 
-		int values[] = new int[5];
-		try {
-			for( int i = 0; i < 5; i++ )
-				values[i] = Integer.parseInt( pieces[i] );
-		} catch (Exception e) { return; }
+        int values[] = new int[5];
+        try {
+            for( int i = 0; i < 5; i++ )
+                values[i] = Integer.parseInt( pieces[i] );
+        } catch (Exception e) {
+            m_botAction.sendPrivateMessage( name, "Input error.  Check and try again." );
+            return;
+        }
 
-		hotSpots.put( message, new HotSpot( values ) );
-		m_botAction.sendPrivateMessage( name, "Hotspot added." );
-	}
+        hotSpots.add( new HotSpot( values ) );
+        m_botAction.sendPrivateMessage( name, "Hotspot added." );
+    }
 
-	public String[] getHelpMessages() {
-		String help[] = {
-		        "HotSpot Module - the neglected, forgotten module",
-		        "!addspot <warpx> <warpy> <radius> <destx> <desty>     - Adds a new hotspot.",
-		        "            Players will warp to the coord <destx>,<desty>",
-		        "            when they enter within <radius> of <warpx>,<warpy>.",
-		        "!watch    - Begin watching all hotspots.",
-		        "(Use !warp module's !where command to locate your hotspots!)"
-		};
-		return help;
-	}
+    public void do_addPrizeHotSpot( String name, String message ) {
 
-	public void cancel() {
-	}
+        String pieces[] = message.split( " " );
+        if( pieces.length != 6 ) return;
+
+        int values[] = new int[6];
+        try {
+            for( int i = 0; i < 6; i++ )
+                values[i] = Integer.parseInt( pieces[i] );
+        } catch (Exception e) {
+            m_botAction.sendPrivateMessage( name, "Input error.  Check and try again." );
+            return;
+        }
+
+        hotSpots.add( new HotSpot( values ) );
+        m_botAction.sendPrivateMessage( name, "Prize hotspot added." );
+    }
+    
+    /**
+     * Clears all hotspots.
+     * @param name
+     */
+    public void do_clearHotSpots( String name ) {
+        hotSpots.clear();
+        m_botAction.sendPrivateMessage( name, "All hotspots cleared." );
+    }
+    
+    public String[] getHelpMessages() {
+        String help[] = {
+                "HotSpot Module - 'Enter one coordinate, warp to another; maybe get a prize.'",
+                "!addspot <warpx> <warpy> <radius> <destx> <desty>     - Adds a new hotspot.",
+                "            Players will warp to the coord <destx>,<desty>",
+                "            when they enter within <radius> of <warpx>,<warpy>.",
+                "!prizespot <warpx> <warpy> <radius> <destx> <desty> <prize>" +
+                "            - Same as !addspot, but players are given <prize> after warping.",
+                "!watch      - Begin watching all hotspots.",
+                "!clearspots - Remove all hotspots.",
+                "(Use !warp module's !where command to locate your hotspots!)"
+        };
+        return help;
+    }
+
+    public void cancel() {
+    }
 }
 
 class HotSpot {
 
-	int x;
-	int y;
-	int r;
-	int x2;
-	int y2;
+    int x;
+    int y;
+    int r;
+    int x2;
+    int y2;
+    int prize;
 
-	public HotSpot( int values[] ) {
-		x = values[0];
-		y = values[1];
-		r = values[2];
-		x2 = values[3];
-		y2 = values[4];
-	}
+    public HotSpot( int values[] ) {
+        if(values.length == 5) {
+            x = values[0];
+            y = values[1];
+            r = values[2];
+            x2 = values[3];
+            y2 = values[4];
+            prize = -1;
+        } else {
+            x = values[0];
+            y = values[1];
+            r = values[2];
+            x2 = values[3];
+            y2 = values[4];
+            prize = values[5];            
+        }
+    }
 
-	public boolean inside( int playerX, int playerY ) {
+    public boolean inside( int playerX, int playerY ) {
 
-		double dist = Math.sqrt( Math.pow( x*16 - playerX , 2 ) + Math.pow( y*16 - playerY , 2 ) );
-		if( dist < r*16 ) return true;
-		else return false;
-	}
+        double dist = Math.sqrt( Math.pow( x*16 - playerX , 2 ) + Math.pow( y*16 - playerY , 2 ) );
+        if( dist < r*16 ) return true;
+        else return false;
+    }
 
-	public int getX() { return x2; }
-	public int getY() { return y2; }
-
+    public int getX() { return x2; }
+    public int getY() { return y2; }
+    public int getPrize() { return prize; }
 }
