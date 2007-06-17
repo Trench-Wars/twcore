4d86522f6b7da4fbc1510c25c263c04985784875
Maverick <Maverick@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-06-17 20:57:43 +0000

    Small update to the runbots.bat with new paths and arguments
    git-svn-id: http://twcore.org/svn/trunk@1573 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/runbots.bat
+++ twcore/src/runbots.bat
@@ -1,3 +1,3 @@
 :Start
-java -Xmx386m -cp twcore.jar;googleapi.jar;mysql-connector-java-3.1.10-bin.jar twcore.core.Start
+java -Xmx64m -cp ..\lib\twcore.jar;..\lib\googleapi.jar;..\lib\mysql-connector-java-3.1.10-bin.jar twcore.core.Start setup.cfg
 GOTO Start
