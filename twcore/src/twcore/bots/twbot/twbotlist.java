430a14da4791eb12bf6a070cb71cbd53916d8b8a
Maverick <Maverick@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-07-17 13:33:09 +0000

    Small update. Returns arena names by PM sorted alphabetically.
    git-svn-id: http://twcore.org/svn/trunk@1654 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/twbot/twbotlist.java
+++ twcore/src/twcore/bots/twbot/twbotlist.java
@@ -1,26 +1,50 @@
 package twcore.bots.twbot;
 
+import java.util.Iterator;
+import java.util.SortedSet;
+
 import twcore.bots.TWBotExtension;
 import twcore.core.events.ArenaList;
 import twcore.core.events.Message;
 
+/**
+ * Module used for listing all the arena names in the current server.
+ * Should be ported into a more general purpose module some day.
+ * 
+ * @author Maverick
+ */
 public class twbotlist extends TWBotExtension {
 
+	private String commander;
+	
 	public twbotlist() { }
 
 	public void handleEvent(ArenaList event)
 	{
+		SortedSet<String> arenaSet = null;
 		String[] arenas = event.getArenaNames();
-		for(int k = 0;k < arenas.length;k++)
-			m_botAction.sendUnfilteredPublicMessage(arenas[k]);
-
-		m_botAction.sendUnfilteredPublicMessage("Arenas listed.");
+		for(int k = 0;k < arenas.length;k++) {
+			arenaSet.add(arenas[k]);
+		}
+		arenas = null;
+		
+		if(commander != null && arenaSet != null && arenaSet.isEmpty()==false) {
+			Iterator<String> it = arenaSet.iterator();
+			while(it.hasNext()) {
+				m_botAction.sendSmartPrivateMessage(commander, it.next());
+			}
+			m_botAction.sendSmartPrivateMessage(commander, "Arenas listed.");
+			commander = null;
+		}
 	}
 
 	public void handleCommand(String name, String message)
 	{
-		if(message.toLowerCase().startsWith("!list"))
+		if(message.toLowerCase().startsWith("!list")) {
+			commander = name;
 			m_botAction.requestArenaList();
+		}
+			
 	}
 
 	public void handleEvent(Message event)
@@ -38,8 +62,8 @@ public class twbotlist extends TWBotExtension {
 	}
 
 	public String[] getHelpMessages() {
-		String[] hi = { "" };
-		return hi;
+		String[] help = { "!list    - Lists all the arena names" };
+		return help;
 	}
 
 	public void cancel() { }
