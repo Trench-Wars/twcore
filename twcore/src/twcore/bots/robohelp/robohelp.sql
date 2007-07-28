271d37272145db102c31df23d24d56c1e2d29984
export <export@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-07-28 22:18:18 +0000

    Added SQL structure dump for RoboHelp
    git-svn-id: http://twcore.org/svn/trunk@1684 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/robohelp/robohelp.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 2.6.1-rc1
+-- http://www.phpmyadmin.net
+-- 
+-- Generation Time: Jul 28, 2007 at 06:17 PM
+-- Server version: 4.1.14
+-- PHP Version: 5.1.6
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tbladvert`
+-- 
+
+CREATE TABLE `tbladvert` (
+  `fnAdvertID` int(11) NOT NULL auto_increment,
+  `fcUserName` varchar(30) NOT NULL default '',
+  `fcEventName` varchar(50) NOT NULL default '',
+  `fcAdvert` varchar(255) NOT NULL default '',
+  `fdTime` datetime NOT NULL default '0000-00-00 00:00:00',
+  PRIMARY KEY  (`fnAdvertID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tblcall`
+-- 
+
+CREATE TABLE `tblcall` (
+  `fnCallID` int(11) unsigned NOT NULL auto_increment,
+  `fcUserName` varchar(30) NOT NULL default '',
+  `fnCount` int(11) NOT NULL default '0',
+  `fdDate` date NOT NULL default '0000-00-00',
+  `fnType` tinyint(1) NOT NULL default '0',
+  `ftUpdated` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`fnCallID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tblwarnings`
+-- 
+
+CREATE TABLE `tblwarnings` (
+  `name` varchar(30) NOT NULL default '',
+  `warning` varchar(255) NOT NULL default '',
+  `timeofwarning` date NOT NULL default '0000-00-00',
+  `staffmember` varchar(30) default NULL,
+  PRIMARY KEY  (`warning`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+        
