086772c73f2418f3c3b9967df2023ee1a6fc3cd4
Ice-demon <Ice-demon@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-08-10 14:19:10 +0000

    Updated  the automated log scanning functionality. It now can turn real time logging on or off to cut the *log spam. It's default logging getfiles subgame.log and parses it for violations and stores them on it's personal log and displays them on an in-game chat on an hourly basis. The log may be out of date at times as subgame after 0:00 takes awhile to refresh subgame.log
    git-svn-id: http://twcore.org/svn/trunk@1741 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/bouncerbotdev/bouncerbotdev.java
+++ twcore/src/twcore/bots/bouncerbotdev/bouncerbotdev.java
@@ -1,5 +1,9 @@
 package twcore.bots.bouncerbotdev;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -7,6 +11,7 @@ import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.TimerTask;
 
 import twcore.core.BotAction;
 import twcore.core.EventRequester;
@@ -16,7 +21,7 @@ import twcore.core.events.LoggedOn;
 import twcore.core.events.Message;
 import twcore.core.events.PlayerEntered;
 import twcore.core.util.Tools;
-import java.util.TimerTask;
+import twcore.core.game.Player;
 
 /**
  * A bot designed to kick players off the server who enter into a particular arena
@@ -37,9 +42,12 @@ public class bouncerbotdev extends SubspaceBot {
     ArrayList<String> ignorelog;
     ArrayList<String> fileNames;
     String bouncemessage;
-    TimerTask logcheck;
-    TimerTask getlogs;
+    TimerTask logCheck;
+    TimerTask getLog;
+    TimerTask archiveLog;
+    File subLog;
     boolean logging;
+    boolean realTimeLogging;
     
     /**
      * Initializes.
@@ -55,6 +63,7 @@ public class bouncerbotdev extends SubspaceBot {
         fileNames = new ArrayList<String>();
         ignorelog = new ArrayList<String>();
         logging = false;
+        realTimeLogging = false;
         EventRequester events = m_botAction.getEventRequester();
         events.request( EventRequester.MESSAGE );
         events.request( EventRequester.PLAYER_ENTERED );
@@ -62,19 +71,43 @@ public class bouncerbotdev extends SubspaceBot {
     }
 
     /**
-     * Places violations a a log file.
+     * Gets the argument tokens from a string.  If there are no colons in the
+     * string then the delimeter will default to space.
+     *
+     * @param string is the string to tokenize.
+     * @return a tokenizer separating the arguments is returned.
+     */
+    
+    public StringTokenizer getArgTokens(String string)	{
+	    if(string.indexOf((int) ',') != -1)
+	      return new StringTokenizer(string, ",");
+	    return new StringTokenizer(string);
+	  }
+    
+    /**
+     * Returns a time stamp.
+     * 
+     * @return a time stamp
+     */
+    
+    public String getTimeStamp()	{
+    	Calendar c = Calendar.getInstance();
+        String timestamp = c.get( Calendar.MONTH ) + "/" + c.get( Calendar.DAY_OF_MONTH )
+        + "/" + c.get( Calendar.YEAR ) + ": " + c.get( Calendar.HOUR ) + ":"
+        + c.get( Calendar.MINUTE ) + ":" + c.get( Calendar.SECOND );
+        return timestamp;
+    }
+    
+    /**
+     * Places violations in a log file.
      * 
      * @param event is the violation to be recorded.
      */
     
-    public void logEvent( String event ){
-        Calendar c = Calendar.getInstance();
-        String timestamp = c.get( Calendar.MONTH ) + "/" + c.get( Calendar.DAY_OF_MONTH )
-        + "/" + c.get( Calendar.YEAR ) + ": " + c.get( Calendar.HOUR ) + ":"
-        + c.get( Calendar.MINUTE ) + ":" + c.get( Calendar.SECOND ) + " - ";
+    public void logEvent( String event )	{
         try{
             PrintWriter out = new PrintWriter( new FileWriter( "bouncerbot.log" ));
-            log.add( timestamp + event );
+            log.add( getTimeStamp() + " - \t" + event );
             for( Iterator<String> i = log.iterator(); i.hasNext(); ){
                 out.println( (String)i.next() );
             }
@@ -109,62 +142,69 @@ public class bouncerbotdev extends SubspaceBot {
         m_opList = m_botAction.getOperatorList();
         logEvent( "Logged in!" );
     }
-
+    
     /**
-     * Handles commands, there is no !help but since you're reading
-     * this, get in the 'know' as this isn't meant to be used
-     * by clueless ERs.
+     * Invites a player
      * 
-     * @param name is the user of the bot.
-     * @param message is the command.
+     * @param sender is the user of the bot.
+     * @param invitee is the player to be invited.
      */
     
-    public void handleCommand( String name, String message ){
-        if( message.startsWith( "!invite " ))	{
-            if( message.length() > 0 ){
-                String invitee = message.substring(8);
-                invitedPlayers.add( invitee.toLowerCase() );
-                m_botAction.sendRemotePrivateMessage( invitee, "You have been invited to " + m_botAction.getArenaName() + "!" );
-                m_botAction.sendPublicMessage( invitee + " has been invited" );
-                logEvent( invitee + " has been invited" );
-            }
+    public void doInvite(String sender, String invitee){
+    	if( invitee.length() > 0 ){
+            invitedPlayers.add( invitee.toLowerCase() );
+            m_botAction.sendRemotePrivateMessage( invitee, "You have been invited to " + m_botAction.getArenaName() + "!" );
+            m_botAction.sendRemotePrivateMessage( sender, invitee + " has been invited" );
+            logEvent( invitee + " has been invited" );
         }
-        else if( message.startsWith( "!message " ))	{
-            bouncemessage = message.substring( 9 );
-            m_botAction.sendRemotePrivateMessage( name, "Message set to: " + bouncemessage );
-        }
-        else if( message.startsWith( "!go " ))	{
-        	m_botAction.changeArena( message.substring( 4 ));
-        	m_botAction.sendRemotePrivateMessage( name,"Going to " + m_botAction.getArenaName());
-        }
-        else if( message.startsWith( "!startlog" ))	
-        {doStartLog(name);}
-        else if( message.startsWith( "!stoplog" ))	
-        {doStopLog(name);}
-        else if( message.startsWith( "!setfiles " ))	
-        {doSetFiles(name,message.substring(10));}
-        else if( message.startsWith( "!clearfiles" ))	
-        {doClearFiles(name);}
-
     }
     
     /**
-     * Gets the argument tokens from a string.  If there are no colons in the
-     * string then the delimeter will default to space.
-     *
-     * @param string is the string to tokenize.
-     * @return a tokenizer separating the arguments is returned.
+     * Clears all guests and ejects them.
+     * 
+     * @param sender is the user of the bot.
      */
     
-    public StringTokenizer getArgTokens(String string)	{
-	    if(string.indexOf((int) ',') != -1)
-	      return new StringTokenizer(string, ",");
-	    return new StringTokenizer(string);
-	  }
+    public void doClearInvites(String sender)	{
+    	invitedPlayers.clear();
+    	Iterator<Player> it = m_botAction.getPlayerIterator();
+    	while (it.hasNext())	{
+    		Player temp = it.next();
+    		String playerName = temp.getPlayerName();
+    		if (m_opList.isOwner(playerName))	{
+    			m_botAction.sendPrivateMessage(playerName, "You are no longer invited!");
+    			m_botAction.sendUnfilteredPrivateMessage(playerName,"*kill");
+    		}
+    	}
+    	m_botAction.sendPrivateMessage( sender, "All invitees have been removed, and ejected!");
+    }
+    
+    /**
+     * Sets the bounce message.
+     * 
+     * @param sender is the user of the bot.
+     * @param message is the bounce message.
+     */
+    
+    public void doBounceMessage(String sender, String message)	{
+    	bouncemessage = message;
+        m_botAction.sendRemotePrivateMessage( sender, "Message set to: " + bouncemessage );
+    }
+    
+    /**
+     * Sends the bot to an arena.
+     * 
+     * @param sender is the user of the bot.
+     * @param message is the area to be entered.
+     */
+    
+    public void doGo (String sender, String message)	{
+    	m_botAction.changeArena( message );
+    	m_botAction.sendRemotePrivateMessage( sender,"Going to " + m_botAction.getArenaName());
+    }
     
     /**
      * Sets files that are to be restricted and logged for violation.
-     * Note: there is no list functionality.
      * 
      * @param sender is the user of the bot
      * @param argString is the string containing the file names + extensions
@@ -177,29 +217,60 @@ public class bouncerbotdev extends SubspaceBot {
 	    	String fileName = argTokens.nextToken();
 	    	int index = fileName.indexOf(".");
 	    	if (index == -1)	{
-	    		m_botAction.sendPrivateMessage(sender, "Invalid file name listed, re-input");
+	    		m_botAction.sendPrivateMessage(sender, "Invalid file name listed, re-input.");
 	    		fileNames.clear();
 	    		return;
 	    	}
 	    	fileNames.add(fileName);
 	    }
-	    m_botAction.sendPrivateMessage(sender, "Restricted files are now: " + fileNames.toString());
+	    String files = fileNames.toString();
+	    m_botAction.sendPrivateMessage(sender, "Restricted files are now: " + files);
+	    logEvent ( "Restrictions changed: " + files );
     }
     
     /**
-     * clears the list of restricted files
+     * Lists the restricted files that are watched
+     * 
+     * @param sender
+     */
+    
+    public void doListFiles(String sender)	{
+    	if (fileNames.isEmpty())
+    		m_botAction.sendPrivateMessage(sender,"There are no restricted files.");
+    	m_botAction.sendPrivateMessage(sender,"The following files are restricted: " + fileNames.toString());
+    }
+    
+    /**
+     * clears the list of restricted files.
+     * 
      * @param sender
      */
     
     public void doClearFiles(String sender)	{
     	fileNames.clear();
     	m_botAction.sendPrivateMessage(sender, "Restricted file list cleared");
+    	logEvent ( "Restrictions changed: " + fileNames.toString() );
     }
     
     /**
-     * Starts the logging functionality, reads the log every 30 seconds.
+     * Turns on/off Real time log violation notification.
+     *  
+     * @param sender is the user of the bot.
+     */
+    
+    public void doRealTime(String sender)	{
+    	if (logging)	{
+    		m_botAction.sendPrivateMessage(sender, "Already logging!...!stoplog then try again.");
+    		return;
+    	}
+    	realTimeLogging = !realTimeLogging;
+    	m_botAction.sendPrivateMessage(sender, "Real Time logging is " + (realTimeLogging? "on" : "off"));
+    }
+    
+    /**
+     * Starts the logging functionality.
      * 
-     * @param sender is thr user of the bot.
+     * @param sender is the user of the bot.
      */
     
     public void doStartLog(String sender)	{
@@ -207,26 +278,41 @@ public class bouncerbotdev extends SubspaceBot {
     		m_botAction.sendPrivateMessage(sender, "Already logging!");
     		return;
     	}
-    		logcheck = new TimerTask()	{
+    		logCheck = new TimerTask()	{
         		public void run()	{
         			m_botAction.sendUnfilteredPublicMessage("*log");
         		}
         		
         	};
-        	getlogs = new TimerTask()	{
+        	getLog = new TimerTask()	{
         		public void run()	{
         			m_botAction.sendUnfilteredPublicMessage("*getfile subgame.log");
         		}
         		
         	};
-        	m_botAction.scheduleTask(logcheck, 2000, 30000);
-        	//m_botAction.scheduleTask(getlogs, 2000, 3600000);
+        	archiveLog = new TimerTask()	{
+        		public void run()	{
+        			try	{
+        			subLog = m_botAction.getDataFile("subgame.log");
+        			String logName = getLogName();
+        			WriteSubLog(logName);
+        			ReadLog(logName);
+        			}
+        			catch (Exception e)	{
+        				m_botAction.sendChatMessage(1,"Failed to write log! (sublog not present?)");
+        			}
+        		}
+        		
+        	};
+        	if(realTimeLogging) m_botAction.scheduleTask(logCheck, 2000, 30000);
+        	m_botAction.scheduleTask(getLog, 2000, 3600000);
+        	m_botAction.scheduleTask(archiveLog, 300000, 3600000);
         	m_botAction.sendPrivateMessage(sender, "Starting to check log");
         	logging = true;
     }
     
     /**
-     * Stops the logging
+     * Stops the logging.
      * 
      * @param sender is the user of the bot.
      */
@@ -236,39 +322,113 @@ public class bouncerbotdev extends SubspaceBot {
     		m_botAction.sendPrivateMessage(sender, "Nothing is being logged!");
     		return;
     	}
-    	logcheck.cancel();
+    	logCheck.cancel();
+    	getLog.cancel();
+    	archiveLog.cancel();
     	m_botAction.sendPrivateMessage(sender, "Logging stopped!");
     	logging = false;
+    }
+    
+    /**
+     * Gets the name of the log from the earliest date on the log.
+     * 
+     * @returns the dated named for the log or null if an exception.
+     */
+    
+    public String getLogName()	{
+    	try	{
+    		BufferedReader  in = new BufferedReader( new FileReader( subLog ));
+        	String line = "";
+        	String target = "";
+        	while( (line = in.readLine()) != null )	{
+        		target = line;
+        	}
+        	in.close();
+        	return subLog.getParent() + File.separatorChar + "Log Archives" + File.separatorChar + target.substring(0, 10) + ".log";
+    	}
+    	catch(Exception e)	{
+        logEvent ( "Failed to read log file " + subLog.getName() );
+        return null;
+    	}
+    }
+    
+    /**
+     * Writes the log into the archive which is 
+     * basically a folder filled with logs.
+     * 
+     * @param logName is the pathname of the log file.
+     */
+    
+    public void WriteSubLog(String logName) {
+    	try	{
+    		File logFile = new File ( logName );
+    		logFile.getParentFile().mkdirs();
+    		String          line;
+            BufferedReader  in = new BufferedReader( new FileReader( subLog ));
+            PrintWriter     out = new PrintWriter( new BufferedWriter ( new FileWriter(logName)));
+            	
+
+            while( (line = in.readLine()) != null )	{
+                out.println(line);
+            }
+            m_botAction.sendChatMessage(1,"Log archive updated. -" + getTimeStamp());
+            in.close();out.close();
+    	}
+    	catch(Exception e){
+            logEvent ("Failed to write log file: " + logName );
+        }
+    }
+    
+    /**
+     * Reads the log for any violations.
+     * 
+     * @param logName is the pathname of the log file.
+     */
+    
+    public void ReadLog(String logName)	{
+    	try	{
+    		String line;
+            BufferedReader  in = new BufferedReader( new FileReader( logName ));
+
+            m_botAction.sendChatMessage(1,"Checking updated log file for violations...");
+            while( (line = in.readLine()) != null )	{
+                handleLog(line);
+            }
+            m_botAction.sendChatMessage(1,"Done!");
+    	}
+    	catch(Exception e){
+            logEvent ("Failed to check log file: " + logName );
+        }
     }
     
     /**
      * Handles the log messages and occasionally the arena message
      * that passes through it's filters.
      * 
-     * @param arenamessage
+     * @param logmessage is the log message to be checked.
      */
     
-    public void handleArenas( String arenamessage )	{ 	
+    public void handleLog( String logmessage )	{ 	
     	if (!logging) return;
-    	if (ignorelog.contains(arenamessage)) return;
+    	if (ignorelog.contains(logmessage)) return;
     	
     	try	{
-    		String command = arenamessage.substring(arenamessage.indexOf("*getfile"));
-        	String violator = arenamessage.substring(arenamessage.indexOf("Ext:")+5, arenamessage.indexOf("(")-1);
+    		String command = logmessage.substring(logmessage.indexOf("*getfile"));
+        	String violator = logmessage.substring(logmessage.indexOf("Ext:")+5, logmessage.indexOf("(")-1);
         	if (m_opList.isOwner(violator))
         			return;
         	else if (m_opList.isSysop(violator))	{
         		if ( command.startsWith("*getfile " + m_botAction.getArenaName()))	{
-        			logEvent( arenamessage );
-        			m_botAction.sendChatMessage(1,violator + " getfile'd " + m_botAction.getArenaName() + "'s files! >>> " + command);
-        			ignorelog.add(arenamessage);
+        			logEvent( logmessage );
+        			m_botAction.sendChatMessage(1,violator + " getfile'd " + m_botAction.getArenaName() + "'s files! -" + getTimeStamp() + " >>> " + command);
+        			ignorelog.add(logmessage);
         		}
         		
         		for ( int i= 0; i<fileNames.size(); i++ )
         		if ( command.equalsIgnoreCase("*getfile " + (String)fileNames.get(i)))	{
-        			logEvent( arenamessage );
-        			m_botAction.sendChatMessage(1,violator + " getfile'd a restricted file! >>> " + command);
-        			ignorelog.add(arenamessage);
+        			logEvent( logmessage );
+        			m_botAction.sendChatMessage(1,violator + " getfile'd a restricted file! -" + getTimeStamp() + " >>> " + command);
+        			ignorelog.add(logmessage);
         		}
         	}
     	}
@@ -291,7 +451,62 @@ public class bouncerbotdev extends SubspaceBot {
         	name = m_botAction.getPlayerName(event.getPlayerID());
         
         if( m_opList.isOwner( name )) handleCommand( name, message );
-        if( type == Message.ARENA_MESSAGE)
-        	handleArenas(message);
+        if( type == Message.ARENA_MESSAGE && realTimeLogging)
+        	handleLog(message);
+    }
+    
+    /**
+     * Handles commands.
+     * 
+     * @param name is the user of the bot.
+     * @param message is the command.
+     */
+    
+    public void handleCommand( String sender, String message ){
+        if( message.startsWith( "!invite " ))
+            doInvite(sender,message.substring(8));
+        else if( message.startsWith( "!message " ))
+        	doBounceMessage(sender,message.substring(9));
+        else if( message.startsWith( "!go " ))
+        	doGo(sender, message.substring(4));
+        else if( message.startsWith( "!help" ))	
+        	m_botAction.privateMessageSpam(sender, getHelpMessages());
+        else if( message.startsWith( "!startlog" ))	
+        	doStartLog(sender);
+        else if( message.startsWith( "!stoplog" ))	
+        	doStopLog(sender);
+        else if( message.startsWith( "!setfiles " ))	
+        	doSetFiles(sender,message.substring(10));
+        else if( message.startsWith( "!listfiles" ))	
+        	doListFiles(sender);
+        else if( message.startsWith( "!clearfiles" ))	
+        	doClearFiles(sender);
+        else if( message.startsWith( "!realtime" ))	
+        	doRealTime(sender);
+
+    }
+    
+    /**
+     * Lists all help messages.
+     * @return returns the help message.
+     */
+    
+    public String[] getHelpMessages()
+    {
+      String[] message =
+      {
+          "!Invite <name>                            -- Invites <name> to " + m_botAction.getArenaName() + " .",
+          "!Message <message>                        -- Sets the bounce message interlopers recieve.",
+          "!Go <arena>                               -- Sends the bot to <arena>.",
+          "!SetFiles <file1>,<file2>,ect...          -- Sets restricted files.",
+          "!ListFiles                                -- Lists curretly restricted files",
+          "!ClearInvites                             -- Clears all guests.",
+          "!ClearFiles                               -- Clears all restricted files",
+          "!RealTime                                 -- Sets real time logging to be on or off.",
+          "!StartLog                                 -- Starts the logging functionality.",
+          "!StopLog                                  -- Stops the logging functionality.",
+          "!help                                     -- Displays this."
+      };
+      return message;
     }
 }
