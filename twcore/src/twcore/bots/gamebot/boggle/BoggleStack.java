ec40fdb17fc4efa477c291404486e9436123ffd7
milosh <milosh@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2007-12-28 07:50:58 +0000

    New boggle module ready for beta testing. Eventually the display will use lvz instead of the current ASCII.
    git-svn-id: http://twcore.org/svn/trunk@2071 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/gamebot/boggle/BoggleStack.java
@@ -0,0 +1,23 @@
+package twcore.bots.gamebot.boggle;
+
+public class BoggleStack {
+	char[][] board;
+	String word;
+	String strFound;
+	Integer row;
+	Integer col;
+	
+	public BoggleStack(char[][] board, String word, String strFound, Integer row, Integer col) {
+		this.board = board;
+		this.word = word;
+		this.strFound = strFound;
+		this.row = row;
+		this.col = col;
+	}
+	
+	public String getWord() {return this.word;}
+	public String getFound() {return this.strFound;}
+	public Integer getRow() {return this.row;}
+	public Integer getCol() {return this.col;}
+	public char[][] getBoard() {return board;}
+}
---
+++ twcore/src/twcore/bots/gamebot/boggle/BoggleXY.java
@@ -0,0 +1,13 @@
+package twcore.bots.gamebot.boggle;
+
+public class BoggleXY {
+    	int x, y;
+
+    	public BoggleXY(int x,int y) {
+    		this.x = x;
+    		this.y = y;
+    	}
+    	
+    	public Integer getX() {return x;}
+    	public Integer getY() {return y;}
+    }
