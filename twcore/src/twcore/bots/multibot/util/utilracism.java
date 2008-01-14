3fc9a71be44eb9ecc5ed7704e30bfd32af67f8d0
Pio <Pio@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-01-14 17:23:54 +0000

    New utility that uses Spy.java (twcore.core.util) to detect racism.
    git-svn-id: http://twcore.org/svn/trunk@2139 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/multibot/util/utilracism.java
@@ -0,0 +1,45 @@
+package twcore.bots.multibot.util;
+
+import twcore.bots.MultiUtil;
+import twcore.core.events.Message;
+import twcore.core.util.ModuleEventRequester;
+import twcore.core.util.Spy;
+
+/**
+ * Created for MultiBot's to detect racism using the Spy class (twcore.core.util).
+ * @author Pio
+ * @version 1.0
+ * Last updated: 1/14/07
+ */
+public class utilracism extends MultiUtil{
+	private Spy watcher;
+	@Override
+	public String[] getHelpMessages() {
+		String[] message =
+	    {
+	      "Racism utility will report a person who uses racist language via ?cheater. The words are in data/racism.cfg.",
+	      "This utility is automaticly active once you load it. Once unloaded it will be deactivated."
+	    };
+
+	    return message;
+	}
+
+	@Override
+	public void init() {
+		watcher = new Spy(m_botAction);
+	}
+
+	@Override
+	public void requestEvents(ModuleEventRequester modEventReq) {
+		// required :o
+	}
+	
+	public void handleEvent(Message event)
+	  {
+		watcher.handleEvent(event);
+	  }
+	
+	public void cancel() {
+	}
+
+}
