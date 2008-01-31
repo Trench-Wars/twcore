81424a9cda32cded977d798a34658b31e09f8b19
Maverick <Maverick@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-01-31 13:50:46 +0000

    Windows batch files to run TWCore can now be found at wiki:RunningOnWindows
    git-svn-id: http://twcore.org/svn/trunk@2264 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/bld.bat
+++
@@ -1,1 +0,0 @@
-@java -classpath BuildTWCore.class; BuildTWCore %1 %2 %3 %4 %5 %6 %7 %8 %9
--- twcore/src/runbots.bat
+++
@@ -1,3 +0,0 @@
-:Start
-java -Xmx64m -cp twcore.jar;..\lib\googleapi.jar;..\lib\mysql-connector-java-5.0.7-bin.jar twcore.core.Start setup.cfg
-GOTO Start
