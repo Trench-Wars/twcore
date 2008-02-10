dcca1d1179295f58e35abacda6e1c3915d3ec8e3
milosh <milosh@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-02-10 20:20:44 +0000

    Updated per request of MMav. Now implements a white list instead of a black one. Javadocs added on all non-inherited methods.
    git-svn-id: http://twcore.org/svn/trunk@2284 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/multibot/util/utilcustom.java
+++ twcore/src/twcore/bots/multibot/util/utilcustom.java
@@ -39,7 +39,11 @@ public class utilcustom extends MultiUtil{
 
 	    return message;
 	}
-
+	
+    /**
+     * Displays a customized help menu for commands created by this utility
+     * @param name - The person requesting the help menu.
+     */
 	public void do_customHelp(String name) {
 		if(commands.size() == 0)return;
 		m_botAction.sendSmartPrivateMessage( name, "+================== Help Menu ===================");
@@ -51,6 +55,12 @@ public class utilcustom extends MultiUtil{
 		m_botAction.sendSmartPrivateMessage( name, "+================================================");
 	}
 	
+	/**
+	 * Adds space padding to the end of a string
+	 * @param length The length you want the string to be after padding
+	 * @param s The string to pad
+	 * @return A new padded string
+	 */
 	public String padString(int length, String s){
 		StringBuilder builder = new StringBuilder();
 		builder.append(s);
@@ -86,6 +96,10 @@ public class utilcustom extends MultiUtil{
 			do_listKeys(name);
 	}
 	
+	/**
+	 * Displays a list of escape keys for use in custom commands and/or messages
+	 * @param name - The person requesting this list
+	 */
 	public void do_listKeys(String name){
 		String msg[] = {
 				"+=============== Escape Keys ===============+",
@@ -103,6 +117,63 @@ public class utilcustom extends MultiUtil{
 		m_botAction.smartPrivateMessageSpam(name, msg);
 	}
 	
+	/**
+	 * A white-list of allowed custom commands.
+	 * @param s - The string
+	 * @return true if the string is allowed. else false.
+	 */
+	public boolean isAllowed(String s){
+		if(s.startsWith("*setship")   ||
+		   s.startsWith("*setfreq")   ||
+		   s.startsWith("*warpto")    ||
+		   s.equals("*scorereset")    ||
+		   s.equals("*spec")          ||
+		   s.equals("*prize #4")      ||//Stealth
+		   s.equals("*prize #5")      ||//Cloak
+		   s.equals("*prize #6")      ||//X-radar
+		   s.equals("*prize #7")      ||//Warp
+		   s.equals("*prize #13")     ||//Full charge
+		   s.equals("*prize #14")     ||//Engine shutdown
+		   s.equals("*prize #15")     ||//Multi-fire
+		   s.equals("*prize #17")     ||//Super
+		   s.equals("*prize #18")     ||//Shields
+		   s.equals("*prize #19")     ||//Shrapnel
+		   s.equals("*prize #20")     ||//Anti-warp
+		   s.equals("*prize #21")     ||//Repel
+		   s.equals("*prize #22")     ||//Burst
+		   s.equals("*prize #23")     ||//Decoy
+		   s.equals("*prize #24")     ||//Thor
+		   s.equals("*prize #25")     ||//Multi-prize
+		   s.equals("*prize #26")     ||//Brick
+		   s.equals("*prize #27")     ||//Rocket
+		   s.equals("*prize #28")     ||//Portal
+		   s.equals("*prize #-4")     ||//Negative Stealth
+		   s.equals("*prize #-5")     ||//Negative Cloak
+		   s.equals("*prize #-6")     ||//Negative X-radar
+		   s.equals("*prize #-7")     ||//Negative Warp
+		   s.equals("*prize #-13")    ||//Negative Full charge
+		   s.equals("*prize #-14")    ||//Negative Engine shutdown
+		   s.equals("*prize #-15")    ||//Negative Multi-fire
+		   s.equals("*prize #-17")    ||//Negative Super
+		   s.equals("*prize #-18")    ||//Negative Shields
+		   s.equals("*prize #-19")    ||//Negative Shrapnel
+		   s.equals("*prize #-20")    ||//Negative Anti-warp
+		   s.equals("*prize #-21")    ||//Negative Repel
+		   s.equals("*prize #-22")    ||//Negative Burst
+		   s.equals("*prize #-23")    ||//Negative Decoy
+		   s.equals("*prize #-24")    ||//Negative Thor
+		   s.equals("*prize #-25")    ||//Negative Multi-prize
+		   s.equals("*prize #-26")    ||//Negative Brick
+		   s.equals("*prize #-27")    ||//Negative Rocket
+		   s.equals("*prize #-28"))     //Negative Portal
+		return true;
+		else return false;
+	}
+	
+	/**
+	 * Allows the bot user to create a description for a custom command that users will see in
+	 * the customized help menu.
+	 */
 	public void do_describe(String name, String message) {
 		int index = message.indexOf(" ");
 		if(index == -1){
@@ -119,6 +190,10 @@ public class utilcustom extends MultiUtil{
 		m_botAction.sendSmartPrivateMessage( name, "Description changed.");
 	}
 	
+	/**
+	 * Allows the bot user to create a custom action for a specified custom command. If the command does
+	 * not exist it is created.
+	 */
 	public void do_addAction(String name, String message) {
 		int index = message.indexOf(" ");
 		if(index == -1){
@@ -127,34 +202,21 @@ public class utilcustom extends MultiUtil{
 		}
 		String command = message.substring(0, index);
 		String msg = message.substring(index + 1);
-		if((msg.startsWith("*warn") ||
-	    		msg.startsWith("*shutup") ||
-	    	    msg.startsWith("*kill") ||
-	    	    msg.startsWith("*mirror") ||
-	    	    msg.startsWith("*sendto") ||
-	    	    msg.startsWith("*ufo") ||
-	    	    msg.startsWith("*putfile") ||
-	    	    msg.startsWith("*getfile") ||
-	    	    msg.startsWith("*sysop") ||
-	    	    msg.startsWith("*smoderator") ||
-	    	    msg.startsWith("*moderator") ||
-	    	    msg.startsWith("*permit") ||
-	    	    msg.startsWith("*revoke") ||
-	    	    msg.startsWith("*points") ||
-	    	    msg.startsWith("*bandwidth") ||
-	    	    msg.startsWith("*lowbandwidth") ||
-	    	    msg.startsWith("*thor") ||
-	    	    msg.startsWith("*super")) &&
-	    	    !opList.isSmod(name)){
-	    		m_botAction.sendSmartPrivateMessage( name, "That command is restricted.");
-	    		return;
-	    	}
+		if(msg.startsWith("*")){
+			if(!isAllowed(msg) && !opList.isSmod(name)){
+				m_botAction.sendSmartPrivateMessage( name, "Command not added; Restricted or unknown.");
+				return;
+			}
+		}		
 		if(!commands.containsKey(command))
 			commands.put(command, new CustomCommand(command));
 		commands.get(command).addMessage(msg);
 		m_botAction.sendSmartPrivateMessage( name, "Action added.");
 	}
 	
+	/**
+	 * Removes a custom action from the specified command.
+	 */
 	public void do_removeAction(String name, String message) {
 		int index = message.indexOf(" ");
 		if(index == -1){
@@ -182,6 +244,9 @@ public class utilcustom extends MultiUtil{
 		}
 	}
 	
+	/**
+	 * Removes an entire custom command and all of its actions.
+	 */
 	public void do_removeCmd(String name, String message){
 		if(commands.containsKey(message)){
 			commands.remove(message);
@@ -191,6 +256,10 @@ public class utilcustom extends MultiUtil{
 			m_botAction.sendSmartPrivateMessage( name, "Specified command not found. Use !listcmd to see a list of registered commands.");
 		}
 	}
+	
+	/**
+	 * Displays a list of all custom commands and their actions(including indices)
+	 */
 	public void do_listCmd(String name){
 		if(commands.size() == 0){
 			m_botAction.sendSmartPrivateMessage( name, "There are no custom commands.");
@@ -209,26 +278,39 @@ public class utilcustom extends MultiUtil{
 		}
 	}
 	
-	
+	/**
+	 * A Custom Command object.
+	 */
 	private class CustomCommand {
 		
-		public ArrayList<String> messages;
-		public String command, description = "No description available.";
+		public ArrayList<String> messages; //All of the actions for this command.
+		public String command; //The name of this command.
+		public String description = "No description available."; //A description of this command for the customized help menu.
 		
 		public CustomCommand(String cmd){
 			command = cmd;
 		}
 		
+		/**
+		 * Changes description for the customized help menu.
+		 */
 		public void describe(String message){
 			description = message;
 		}
 		
+		/**
+		 * Adds an action to this command.
+		 */
 		public void addMessage(String message){
 			if(messages == null)
 				messages = new ArrayList<String>();
 			messages.add(message);
 		}
 		
+		/**
+		 * Removes an action from this command
+		 * @param index - the action's index as shown by !listcmd
+		 */
 		public void removeMessage(int index){
 			if(messages != null)
 				messages.remove(index);
@@ -236,14 +318,28 @@ public class utilcustom extends MultiUtil{
 				messages = null;
 		}
 		
+		/**
+		 * Checks to see if the given index exists
+		 * @return true if it does. else false.
+		 */
 		public boolean hasIndex(int index){
 			return messages.get(index) != null;
 		}
 		
+		/**
+		 * Get the entire ArrayList of actions for this command.
+		 * @return the ArrayList
+		 */
 		public ArrayList<String> getMessages(){
 			return messages;
 		}
 		
+		/**
+		 * Replaces escape keys when messaging a user who has used this command
+		 * @param name - The user accessing this command
+		 * @param message - The message/action to be sent to the user
+		 * @return A string with objects instead of escape keys
+		 */
 		public String replaceKeys(String name, String message){
 			Player p = m_botAction.getPlayer(name);
 			if(message.contains("&player"))
@@ -277,6 +373,9 @@ public class utilcustom extends MultiUtil{
 			return message;
 		}
 		
+		/**
+		 * Returns the name of a ship 0-spectator, 1-8 ships
+		 */
 		public String getShipNames(int ship){
 			String message = "-Unknown-";
 			switch(ship){
@@ -293,6 +392,10 @@ public class utilcustom extends MultiUtil{
 			return message;
 		}
 		
+		/**
+		 * Sends all messages/actions listed in this command to the user.
+		 * @param name - the user.
+		 */
 		public void message(String name){
 			Iterator<String> it = messages.iterator();
 			while( it.hasNext() ) {
--- twcore/src/twcore/bots/multibot/util/utilhotspots.java
+++ twcore/src/twcore/bots/multibot/util/utilhotspots.java
@@ -17,7 +17,6 @@ import twcore.core.util.ModuleEventRequester;
  *Modified the hotspots module with a better UI and fixed
  *the timer task errors. Should be working properly.
  */
-
 public class utilhotspots extends MultiUtil {
 
 	OperatorList opList;
@@ -33,7 +32,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * initializes.
      */
-
     public void init() {
     	recentContacts = new ArrayList<String>();
     	opList = m_botAction.getOperatorList();
@@ -47,7 +45,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * requests events.
      */
-
     public void requestEvents( ModuleEventRequester modEventReq ) {
     	modEventReq.request(this, EventRequester.PLAYER_POSITION );
     }
@@ -55,7 +52,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * Handles messages
      */
-
     public void handleEvent( Message event ){
 
         String message = event.getMessage();
@@ -72,7 +68,6 @@ public class utilhotspots extends MultiUtil {
      * @param sender is the user of the bot.
      * @param message is the command.
      */
-
     public void handleCommand( String sender, String message ) {
     	String msg = message;
     	message = message.toLowerCase();
@@ -105,7 +100,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * Handles player positions
      */
-
     public void handleEvent( PlayerPosition event ) {
         String name = m_botAction.getPlayerName(event.getPlayerID());
         if(name == null || recentContacts.contains(name))return;
@@ -128,7 +122,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * initializes timer task.
      */
-
     public void watch()	{
     	changeTask = new TimerTask() {
             public void run() {
@@ -150,7 +143,6 @@ public class utilhotspots extends MultiUtil {
      * Lists spots.
      * @param sender is the user of the bot
      */
-
     public void do_ListSpot(String sender)	{
     	Iterator<HotSpot> it = hotSpots.iterator();
     	if(!it.hasNext()) {m_botAction.sendPrivateMessage(sender,"No spots loaded!");return;}
@@ -165,8 +157,7 @@ public class utilhotspots extends MultiUtil {
      * Adds a message to a spot
      * @param sender is the user of the bot
      * @param message is the index of the spot and the message to add
-     */
-    
+     */   
     public void do_addMessage( String sender, String message ) {
     	String spot = message.substring(0, 1);
     	if(message.indexOf(" ") == -1)return;
@@ -184,31 +175,68 @@ public class utilhotspots extends MultiUtil {
     		m_botAction.sendPrivateMessage( sender, "The given HotSpot does not exist.");
     		return;
     	}
-    	if((msg.startsWith("*warn") ||
-    		msg.startsWith("*shutup") ||
-    	    msg.startsWith("*kill") ||
-    	    msg.startsWith("*mirror") ||
-    	    msg.startsWith("*sendto") ||
-    	    msg.startsWith("*ufo") ||
-    	    msg.startsWith("*putfile") ||
-    	    msg.startsWith("*getfile") ||
-    	    msg.startsWith("*sysop") ||
-    	    msg.startsWith("*smoderator") ||
-    	    msg.startsWith("*moderator") ||
-    	    msg.startsWith("*permit") ||
-    	    msg.startsWith("*revoke") ||
-    	    msg.startsWith("*points") ||
-    	    msg.startsWith("*bandwidth") ||
-    	    msg.startsWith("*lowbandwidth") ||
-    	    msg.startsWith("*thor") ||
-    	    msg.startsWith("*super")) &&
-    	    !opList.isSmod(sender)){
-    		m_botAction.sendSmartPrivateMessage( sender, "That command is restricted. There is really no reason to use it with a hot spot.");
-    		return;
-    	}
+    	if(msg.startsWith("*")){
+			if(!isAllowed(msg) && !opList.isSmod(sender)){
+				m_botAction.sendSmartPrivateMessage( sender, "Command not added; Restricted or unknown.");
+				return;
+			}
+		}
     	hotSpots.elementAt(index).addMessage(msg);
     	m_botAction.sendSmartPrivateMessage( sender, "Message added to HotSpot at index " + index );
     }
+    
+    /**
+	 * A white-list of allowed custom commands.
+	 * @param s - The string
+	 * @return true if the string is allowed. else false.
+	 */
+	public boolean isAllowed(String s){
+		if(s.startsWith("*setship")   ||
+		   s.startsWith("*setfreq")   ||
+		   s.startsWith("*warpto")    ||
+		   s.equals("*scorereset")    ||
+		   s.equals("*spec")          ||
+		   s.equals("*prize #4")      ||//Stealth
+		   s.equals("*prize #5")      ||//Cloak
+		   s.equals("*prize #6")      ||//X-radar
+		   s.equals("*prize #7")      ||//Warp
+		   s.equals("*prize #13")     ||//Full charge
+		   s.equals("*prize #14")     ||//Engine shutdown
+		   s.equals("*prize #15")     ||//Multi-fire
+		   s.equals("*prize #17")     ||//Super
+		   s.equals("*prize #18")     ||//Shields
+		   s.equals("*prize #19")     ||//Shrapnel
+		   s.equals("*prize #20")     ||//Anti-warp
+		   s.equals("*prize #21")     ||//Repel
+		   s.equals("*prize #22")     ||//Burst
+		   s.equals("*prize #23")     ||//Decoy
+		   s.equals("*prize #24")     ||//Thor
+		   s.equals("*prize #25")     ||//Multi-prize
+		   s.equals("*prize #26")     ||//Brick
+		   s.equals("*prize #27")     ||//Rocket
+		   s.equals("*prize #28")     ||//Portal
+		   s.equals("*prize #-4")     ||//Negative Stealth
+		   s.equals("*prize #-5")     ||//Negative Cloak
+		   s.equals("*prize #-6")     ||//Negative X-radar
+		   s.equals("*prize #-7")     ||//Negative Warp
+		   s.equals("*prize #-13")    ||//Negative Full charge
+		   s.equals("*prize #-14")    ||//Negative Engine shutdown
+		   s.equals("*prize #-15")    ||//Negative Multi-fire
+		   s.equals("*prize #-17")    ||//Negative Super
+		   s.equals("*prize #-18")    ||//Negative Shields
+		   s.equals("*prize #-19")    ||//Negative Shrapnel
+		   s.equals("*prize #-20")    ||//Negative Anti-warp
+		   s.equals("*prize #-21")    ||//Negative Repel
+		   s.equals("*prize #-22")    ||//Negative Burst
+		   s.equals("*prize #-23")    ||//Negative Decoy
+		   s.equals("*prize #-24")    ||//Negative Thor
+		   s.equals("*prize #-25")    ||//Negative Multi-prize
+		   s.equals("*prize #-26")    ||//Negative Brick
+		   s.equals("*prize #-27")    ||//Negative Rocket
+		   s.equals("*prize #-28"))     //Negative Portal
+		return true;
+		else return false;
+	}
     
     /**
      * Removes a message from a spot
@@ -244,8 +272,7 @@ public class utilhotspots extends MultiUtil {
      * Lists the messages of a spot
      * @param sender is the user of the bot
      * @param message is the index of the spot
-     */
-    
+     */  
     public void do_listMessages( String sender, String message ) {
     	int index;
     	try {
@@ -278,15 +305,14 @@ public class utilhotspots extends MultiUtil {
      * @param sender is the user of the bot.
      * @param message is the hotspot.
      */
-
     public void do_addHotSpot( String sender, String message ) {
 
         String pieces[] = message.split( " " );
-        if( pieces.length != 3 ) return;//Changed from 5 to 3 - milosh 2.9.08
+        if( pieces.length < 3 || pieces.length == 4 || pieces.length > 6) return;
 
-        int values[] = new int[3];//Changed from 5 to 3 - milosh 2.9.08
+        int values[] = new int[pieces.length];
         try {
-            for( int i = 0; i < 3; i++ )//Changed from 5 to 3 - milosh 2.9.08
+            for( int i = 0; i < pieces.length; i++ )
                 values[i] = Integer.parseInt( pieces[i] );
         } catch (Exception e) {
             m_botAction.sendPrivateMessage( sender, "Input error.  Check and try again." );
@@ -322,7 +348,6 @@ public class utilhotspots extends MultiUtil {
      * @param sender is the user of the bot
      * @param message is the command
      */
-
     public void do_switchTime( String sender, String message ) {
         int time = switchTime;
         try {
@@ -344,6 +369,11 @@ public class utilhotspots extends MultiUtil {
         m_botAction.sendPrivateMessage( sender, "Switch time set to " + time );
     }
 
+    /**
+     * Changes the amount of time a user must wait to get a response from a hotspot after an initial response.
+     * @param sender is the user of the bot
+     * @param message is the command
+     */
     public void do_repeatTime( String sender, String message ) {
     	int time = repeatTime;
         try {
@@ -364,7 +394,6 @@ public class utilhotspots extends MultiUtil {
     /**
      * Returns help message
      */
-
     public String[] getHelpMessages() {
         String help[] = {
                 "HotSpot Module          - Enter one coordinate, warp to another; maybe get a prize.'",
@@ -389,6 +418,9 @@ public class utilhotspots extends MultiUtil {
         m_botAction.resetReliablePositionUpdating();
     }
     
+    /**
+     * A timer object created so that players sitting on a hotspot will not cause packet flooding.
+     */
     private class ContactTimer {
     	private Timer clock;
     	private TimerTask task;
@@ -412,7 +444,6 @@ public class utilhotspots extends MultiUtil {
 /**
  *Hot spot class that holds all related values
  */
-
 class HotSpot {
 
     int x;
@@ -420,6 +451,10 @@ class HotSpot {
     int r;
     ArrayList<String> messages;
 
+    /**
+     * Creates a new hotspot with the array of values. You can still use 5/6 values for warping/prizing, but
+     * only 3 values are needed to create a working hotspot.
+     */
     public HotSpot( int values[] ) {
     	if(values.length == 3){
             x = values[0];
@@ -441,18 +476,27 @@ class HotSpot {
     	}
     }
     
+    /**
+     * Add a message/command a player will receive when coming into contact with this spot.
+     */
     public void addMessage( String message ){
     	if(messages == null)
     		messages = new ArrayList<String>();
     	messages.add(message);
     }
     
+    /**
+     * Get the message at a specified index. If there is none return null.
+     */
     public String getMessage( int index ){
     	try{
-    	return messages.get(index);
-    	}catch(Exception e){return null;}
+    		return messages.get(index);
+    	}catch(Exception e){return null;}//throws ArrayIndexOutOfBounds
     }
     
+    /**
+     * Remove a message/command a player will receive when coming into contact with this spot.
+     */
     public void removeMessage( int index ){
     	try{
     		messages.remove(index);
@@ -461,12 +505,17 @@ class HotSpot {
     	} catch(Exception e){}
     }
     
+    /**
+     * Get the ArrayList of messages for this spot.
+     */
     public ArrayList<String> getMessages(){
     	return messages;
     }
     
+    /**
+     * Return true if the given X,Y is touching this spot. Else return false.
+     */
     public boolean inside( int playerX, int playerY ) {
-
         double dist = Math.sqrt( Math.pow( x*16 - playerX , 2 ) + Math.pow( y*16 - playerY , 2 ) );
         if( Math.round(dist) <= r*16 ) return true;
         else return false;
