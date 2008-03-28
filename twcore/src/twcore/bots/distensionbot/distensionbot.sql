2e3bd0fcb7d05d5054ea7b30a131ad015bbc6383
dugwyler <dugwyler@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-03-28 14:58:40 +0000

    Added 9th ship, 15th upgrade, + battles won and kill message sending for player.
    git-svn-id: http://twcore.org/svn/trunk@2434 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/distensionbot/distensionbot.sql
+++ twcore/src/twcore/bots/distensionbot/distensionbot.sql
@@ -9,15 +9,18 @@ CREATE TABLE `tblDistensionPlayer` (
   `fcName` varchar(25) NOT NULL default '',
   `fnArmyID` smallint(5) unsigned NOT NULL default '0',
   `fnTime` smallint(5) NOT NULL default '0',
+  `fnBattlesWon` mediumint(8) unsigned NOT NULL default '0',
   `fcBanned` enum('n','y') NOT NULL default 'n',
+  `fcSendKillMsg` enum('n','y') NOT NULL default 'y',
   `fcShip1` enum('n','y') NOT NULL default 'y',
   `fcShip2` enum('n','y') NOT NULL default 'n',
   `fcShip3` enum('n','y') NOT NULL default 'n',
   `fcShip4` enum('n','y') NOT NULL default 'n',
-  `fcShip5` enum('n','y') NOT NULL default 'n',
+  `fcShip5` enum('n','y') NOT NULL default 'y',
   `fcShip6` enum('n','y') NOT NULL default 'n',
   `fcShip7` enum('n','y') NOT NULL default 'n',
   `fcShip8` enum('n','y') NOT NULL default 'n',
+  `fcShip9` enum('n','y') NOT NULL default 'n',
   PRIMARY KEY  (`fnID`),
   UNIQUE KEY `fcName` (`fcName`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Player data for the progressive RPG-style war game, Distensi' AUTO_INCREMENT=640 ;
@@ -48,6 +51,7 @@ CREATE TABLE `tblDistensionShip` (
   `fnStat12` tinyint(3) unsigned NOT NULL default '0',
   `fnStat13` tinyint(3) unsigned NOT NULL default '0',
   `fnStat14` tinyint(3) unsigned NOT NULL default '0',
+  `fnStat15` tinyint(3) unsigned NOT NULL default '0',
   PRIMARY KEY  (`fnPlayerID`,`fnShipNum`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Distension (''RPG''-like pub) ship settings';
 
@@ -67,3 +71,34 @@ CREATE TABLE `tblDistensionArmy` (
   PRIMARY KEY  (`fnArmyID`),
   UNIQUE KEY `fnArmyName` (`fcArmyName`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblDistensionNews`
+--
+
+CREATE TABLE `tblDistensionNews` (
+  `fnPostID` mediumint(8) unsigned NOT NULL auto_increment,
+  `fcTitle` varchar(40) NOT NULL default '',
+  `fdDate` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `ftBody` text NOT NULL,
+  `fcAuthorName` varchar(40) NOT NULL default '',
+  PRIMARY KEY  (`fnPostID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblDistensionUpgrade`
+--
+
+CREATE TABLE `tblDistensionUpgrade` (
+  `fnShipNum` smallint(5) unsigned NOT NULL,
+  `fnUpgradeNum` smallint(5) unsigned NOT NULL,
+  `fcDesc` varchar(30) NOT NULL,
+  `fnPrizeNum` smallint(6) NOT NULL,
+  `fnPointsReq` varchar(50) NOT NULL default '0' COMMENT 'Comma-separated integers',
+  `fnRankReq` varchar(50) NOT NULL default '0' COMMENT 'Comma-separated integers',
+  KEY `fnShipNum` (`fnShipNum`,`fnUpgradeNum`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Stores static information on each upgrade';
