8566208c819745b5fccb8e71e007e02549c47309
milosh <milosh@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-05-01 02:36:10 +0000

    Ticket #259
    git-svn-id: http://twcore.org/svn/trunk@2567 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/messagebot/messagebot.java
+++ twcore/src/twcore/bots/messagebot/messagebot.java
@@ -363,8 +363,14 @@ public class messagebot extends SubspaceBot
     	}
 
     	Channel c = channels.get(channel);
-    	if(c.isOp(name) || m_botAction.getOperatorList().isHighmod(name) || ops.contains(name.toLowerCase()))
-    		c.messageChannel(name, pieces[1]);
+    	if(c.isOp(name) || m_botAction.getOperatorList().isHighmod(name) || ops.contains(name.toLowerCase())){
+    	    if(pieces[1].length() > 200){
+    	        m_botAction.sendSmartPrivateMessage( name, "That message is too long to send. Please create a message of 200 characters or less.");
+    	        return;
+    	    }
+    	    else
+    	        c.messageChannel(name, pieces[1]);
+    	}
     	else
     		m_botAction.sendSmartPrivateMessage(name, "You do not have permission to do that on this channel.");
     }
