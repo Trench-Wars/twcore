6f5b36092eb78e7952cd7edab1d9cf33d353bcb5
Maverick <Maverick@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2008-09-07 20:39:21 +0000

    Small change to the arena packet & handling
    git-svn-id: http://twcore.org/svn/trunk@2838 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/events/ArenaList.java
+++ twcore/src/twcore/core/events/ArenaList.java
@@ -9,15 +9,12 @@ import twcore.core.util.ByteArray;
 
 /*
 47 - Arena list
-Field    Length    Description
-0        1        Type byte
-
-Repeats until end of packet:
-1        ?        Arena name
-?        1        \0
-?        1        Arena size
-?        1        \0
-End of repeated section
+  
+Field   Length  Description
+0       1       Type byte
+The following are repeated until the end of the message
+1       ...\0   Arena name
+?       2       Arena population
 */
 
 public class ArenaList extends SubspaceEvent {
@@ -34,10 +31,10 @@ public class ArenaList extends SubspaceEvent {
             name = array.readNullTerminatedString( i );
             i += name.length();
             i += 1; // For the terminating null
-            size = array.readByte( i );
-            i += 1; // For the size
-            i += 1; // For the terminating null
-            m_arenaList.put( name.toLowerCase(), new Integer( size ) );
+            size = array.readLittleEndianShort( i );
+            size = Math.abs(size);  // negative to positive
+            i += 2; // For the size
+            m_arenaList.put( name.toLowerCase(), size );
         }
     }
 
@@ -51,7 +48,7 @@ public class ArenaList extends SubspaceEvent {
     }
 
     public int getSizeOfArena( String arenaName ){
-        return Math.abs( m_arenaList.get( arenaName.toLowerCase() ).intValue() );
+        return m_arenaList.get( arenaName.toLowerCase() );
     }
 
     public String getCurrentArenaName(){
