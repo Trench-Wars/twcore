5aeec748e38e68f3c5a130d81acf6c3cb21a5aec
Maverick <Maverick@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2009-04-26 13:08:36 +0000

    Protocol/Core change! The protocol made a mistake on the ScoreUpdate package - the flag points and kill points were reversed.
    I checked the impact of this mistake/change: no bots but the pubstats were using this ScoreUpdate packet.
    
    git-svn-id: http://twcore.org/svn/trunk@3276 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/events/ScoreUpdate.java
+++ twcore/src/twcore/core/events/ScoreUpdate.java
@@ -10,8 +10,8 @@ import twcore.core.util.ByteArray;
  * +-------------------------+
  * |0       1    Type byte	 |
  * |1       2    Player ident|
- * |3       4    Flag points |
  * |7       4    Kill points |
+ * |3       4    Flag points |
  * |11      2    Wins        |
  * |13      2    Losses      |
  * +-------------------------+</code></pre>
@@ -30,8 +30,8 @@ public class ScoreUpdate extends SubspaceEvent {
      */
     public ScoreUpdate(ByteArray array){
         m_playerID = array.readLittleEndianShort( 1 );
-        m_flagPoints = array.readLittleEndianInt( 3 );
-        m_killPoints = array.readLittleEndianInt( 7 );
+        m_killPoints = array.readLittleEndianInt( 3 );
+        m_flagPoints = array.readLittleEndianInt( 7 );
         m_wins = array.readLittleEndianShort( 11 );
         m_losses = array.readLittleEndianShort( 13 );
     }
