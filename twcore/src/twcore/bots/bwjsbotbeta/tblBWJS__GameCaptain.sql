98ac2e7d81a243693e6cbcc5b0203a46eb35ee20
fantus <fantus@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2009-08-28 21:49:26 +0000

    BWJSBot with stats (still beta)
    git-svn-id: http://twcore.org/svn/trunk@3379 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/bwjsbotbeta/tblBWJS__GameCaptain.sql
@@ -0,0 +1,14 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+USE `twcore`;
+
+CREATE TABLE `tblBWJS__GameCaptain` (
+  `matchID` BIGINT unsigned default NULL,
+  `userID` BIGINT unsigned default NULL,
+  `team` SMALLINT(1) default 0,
+  `startTime` BIGINT default NULL,
+  `endTime` BIGINT default NULL,
+  KEY  (`matchID`),
+  KEY `userID` (`userID`),
+  KEY `team` (`team`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
