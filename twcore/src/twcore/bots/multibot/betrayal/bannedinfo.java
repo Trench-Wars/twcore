2e12d154a717e412ebdf806f1f71de5d8da4c9a6
Dexter <Dexter@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-01-23 20:00:25 +0000

    #125 match file betrayal.jav
    git-svn-id: http://twcore.org/svn/trunk@3531 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/multibot/betrayal/bannedinfo.java
@@ -0,0 +1,37 @@
+package twcore.bots.multibot.betrayal;
+
+public class bannedinfo {
+	
+	private String playerName;
+	private int Freq;
+	private int oldX, oldY;
+	
+	
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public int getFreq() {
+		return Freq;
+	}
+	public void setFreq(short Freq) {
+		this.Freq = Freq;
+	}
+	public int getOldX() {
+		return oldX;
+	}
+	public void setOldX(int oldX) {
+		this.oldX = oldX;
+	}
+	public int getOldY() {
+		return oldY;
+	}
+	public void setOldY(int oldY) {
+		this.oldY = oldY;
+	}
+	
+	
+	
+}
