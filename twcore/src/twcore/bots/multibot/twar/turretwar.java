2d3d7edae79fc8410838c30211730396c6c57637
Dexter <Dexter@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-04-17 23:50:20 +0000

    some updates on #483 match file
    git-svn-id: http://twcore.org/svn/trunk@3574 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/multibot/twar/turretwar.java
+++ twcore/src/twcore/bots/multibot/twar/turretwar.java
@@ -2,10 +2,8 @@ package twcore.bots.multibot.twar;
 
 public interface turretwar {
 
-    public void cmd_start       (String name, String message);
-    public void cmd_addTerr     (String name, String message);
+     public void cmd_addTerr     (String name, String message);
     public void cmd_switchTerr  (String name, String message);
-    public void cmd_removeTerr  ();
     public void cmd_stop        (String name, String message);
     public void cmd_warp        (String name, String message);
     public void cmd_firstShip   (String name, String message);
