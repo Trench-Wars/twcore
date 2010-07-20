2328924cb2e15464b2b979778f7a81e330871cb3
Arobas+ <Arobas+@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-07-20 01:25:05 +0000

    git-svn-id: http://twcore.org/svn/trunk@3842 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/Reloadable.java
+++ twcore/src/twcore/core/Reloadable.java
@@ -1,5 +1,9 @@
 package twcore.core;
 
+/**
+ * If a bot use a configuration file, it should implement this interface
+ * The command "!reloadconf <bottype>" will call reload()
+ */
 public interface Reloadable {
 
 	public void reload();
