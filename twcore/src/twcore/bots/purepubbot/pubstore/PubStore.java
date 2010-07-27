534fdf1112c1b9af4951dbe570f13a9bd0cb7315
Dexter <Dexter@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-07-27 21:32:37 +0000

    fixed price
    git-svn-id: http://twcore.org/svn/trunk@3920 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/purepubbot/pubstore/PubStore.java
+++ twcore/src/twcore/bots/purepubbot/pubstore/PubStore.java
@@ -46,7 +46,7 @@ public class PubStore {
         PubItem itemsubject9 = new LeviHouseOff(bot, "leviathan3", 0, 50);
         PubItem itemsubject10 = new JavelinHouseOff(bot, "javelin3", 0, 50);
         PubItem itemsubject11 = new WarbirdHouseOff(bot, "warbirdoff", 0, 500);
-        PubItem item1 = new Repel("repel", 21, 0);
+        PubItem item1 = new Repel("repel", 21, 50);
         PubItem item2 = new Super("super", 17, 5000);
         PubItem item3 = new Rocket("rocket", 27, 50);
         PubItem item4 = new Burst("burst", 22, 250);
