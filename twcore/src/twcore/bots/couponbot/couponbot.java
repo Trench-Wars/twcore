77c907dd44ea098a9563c8dfd01b3bdb4d196abc
Arobas+ <Arobas+@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-10-19 10:40:08 +0000

    fix to couponbot
    git-svn-id: http://twcore.org/svn/trunk@4145 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/couponbot/couponbot.java
+++ twcore/src/twcore/bots/couponbot/couponbot.java
@@ -96,6 +96,8 @@ public class couponbot extends SubspaceBot {
     	if (event.getObject() instanceof IPCMessage) {
     		
     		IPCMessage ipc = (IPCMessage) event.getObject();
+    		if (!ipc.getRecipient().equals("couponbot"))
+    			return;
 
     		if (ipc.getMessage().startsWith("couponsuccess:")) {
     			
