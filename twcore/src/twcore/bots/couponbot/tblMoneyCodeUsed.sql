4b9bde93eb999fdbacfea36d92f7f621eeec83a9
Arobas+ <Arobas+@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2010-10-25 22:47:39 +0000

    CouponBot now integrated with PubSystem
    git-svn-id: http://twcore.org/svn/trunk@4213 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/couponbot/tblMoneyCodeUsed.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tblMoneyCodeUsed` (
-  `fnMoneyCodeUserId` int(11) NOT NULL AUTO_INCREMENT,
-  `fnMoneyCodeId` int(11) NOT NULL,
-  `fcName` varchar(23) NOT NULL COMMENT 'Who used this code',
-  `fdCreated` datetime NOT NULL,
-  `ftUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`fnMoneyCodeUserId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
