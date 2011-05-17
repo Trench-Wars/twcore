6d2cfdd8d285aaae3cccd67efb10ca546dd0c472
JoyRider <JoyRider@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2011-05-17 13:15:34 +0000

    added: An utility to drop the ball at a given location as request by Dezmond.
    git-svn-id: http://twcore.org/svn/trunk@4944 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/multibot/util/utilballdrop.java
@@ -0,0 +1,123 @@
+package twcore.bots.multibot.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+import twcore.bots.MultiUtil;
+import twcore.core.EventRequester;
+import twcore.core.events.BallPosition;
+import twcore.core.events.Message;
+import twcore.core.events.TurretEvent;
+import twcore.core.game.Player;
+import twcore.core.util.ModuleEventRequester;
+
+/**
+ * 
+ * 
+ * @author JoyRider
+ *
+ */
+
+public class utilballdrop extends MultiUtil {
+    private int m_lastBallTimestamp;
+
+    /**
+     * Initializes global variables.
+     */
+    public void init() {
+        m_lastBallTimestamp = 0;
+    }
+
+    /**
+     * Requests events.
+     */
+    public void requestEvents( ModuleEventRequester events ) {
+        events.request(this, EventRequester.BALL_POSITION);
+        // events.request(this, EventRequester.MESSAGE); // required?
+    }
+
+    /**
+     * Handles ball position updates, required to get the correct ball timestamp
+     */
+    public void handleEvent( BallPosition event ) {
+        m_lastBallTimestamp = event.getTimeStamp();
+    }
+
+    /**
+     * Handles all message events.
+     */
+    public void handleEvent( Message event ) {
+        String message = event.getMessage();
+        String playerName = m_botAction.getPlayerName(event.getPlayerID());
+
+        if( event.getMessageType() == Message.PRIVATE_MESSAGE ) {
+            if( m_opList.isER(playerName) ) {
+                if( message.startsWith("!ball") ) {
+                    checkDropBall(playerName, message.substring(6));
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks for correct coordinates for !ball command
+     * 
+     * @param sender Player that sends the request
+     * @param coords
+     */
+    public void checkDropBall( String sender, String coords ) {
+        short x = -1, y = -1;
+        boolean valid = true;
+        String[] vars = coords.split("(:| )");
+
+        if( vars.length < 2 )
+            valid = false;
+
+        if( valid ) {
+            try {
+                x = Short.parseShort(vars[0]);
+                y = Short.parseShort(vars[1]);
+            } catch( NumberFormatException e ) {
+                valid = false;
+            }
+        }
+
+        if( valid ) {
+            if( (x <= 0 || x > 1024) || (y <= 0 || y > 1024) )
+                valid = false;
+        }
+
+        if( !valid )
+            m_botAction.sendPrivateMessage(sender,
+                    "Invalid usage, the correct usage is !ball <x>:<y>. Coordinates range from 1 to 1024");
+        else {
+            doDropBall(x, y);
+            m_botAction.sendPrivateMessage(sender, "Ball dropped at " + x + " " + y);
+        }
+    }
+
+    /**
+     * Executes the ball drop
+     * 
+     * @param x X coord of ball drop
+     * @param y Y coord of ball drop
+     */
+    public void doDropBall( short x, short y ) {
+        m_botAction.getShip().setShip(0);
+        m_botAction.getShip().move(x << 4, y << 4);
+        m_botAction.getBall((byte)0, m_lastBallTimestamp);
+        m_botAction.getShip().setShip(8);
+    }
+
+    /**
+     * Return's the bot's help messages.
+     */
+    public String[] getHelpMessages() {
+        String[] message = { "!ball <x>:<y> -- Drops the soccer ball at location <x>,<y>. Coordinates range from 1 to 1024", };
+        return message;
+    }
+
+}
