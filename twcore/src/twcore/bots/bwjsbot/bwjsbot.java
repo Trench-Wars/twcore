0fd501bf630fa460cb0b7ef1f598bf06cca8cc34
dezmond <dezmond@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2011-11-24 19:29:15 +0000

    lol
    git-svn-id: http://twcore.org/svn/trunk@6095 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/bwjsbot/bwjsbot.java
+++ twcore/src/twcore/bots/bwjsbot/bwjsbot.java
@@ -1574,7 +1574,7 @@ public class bwjsbot extends SubspaceBot {
             }
             
             /* Check if sub is a bot */
-            if (m_botAction.getOperatorList().isSysopExact(playerBnew.getPlayerName()) && !playerBnew.getPlayerName().equalsIgnoreCase("Pure_luck") && !playerBnew.getPlayerName().equalsIgnoreCase("PinkSTAR")) {
+            if (m_botAction.getOperatorList().isSysopExact(playerBnew.getPlayerName()) && !playerBnew.getPlayerName().equalsIgnoreCase("Pure_luck") && !playerBnew.getPlayerName().equalsIgnoreCase("PinkSTAR") && !playerBnew.getPlayerName().equalsIgnoreCase("Demonic") && !playerBnew.getPlayerName().equalsIgnoreCase("M_M God")) {
                 m_botAction.sendPrivateMessage(name, "Error: Bots are not allowed to play.");
                 return;
             }
