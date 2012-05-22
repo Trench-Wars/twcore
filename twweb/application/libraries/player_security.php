75da06bd7f83e0c48b20f32ba7fb9ca4a854b020
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-05-22 20:06:07 +0000

    Initial player login security class.
    git-svn-id: http://twcore.org/svn/trunk@6478 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twweb/application/libraries/player_security.php
@@ -0,0 +1,25 @@
+<?php
+	/*	Basic security class o)-<| */
+	class Player_security {
+		public function __construct() {
+			$this->ci =& get_instance();
+		}
+
+		public function isLoggedIn() {
+			if($this->ci->session->userdata('logged_in') == 'true') return true;
+			else return false;
+		}
+
+		public function login($playerData) {
+			$this->ci->session->set_userdata(array(
+				'logged_in'   => 'true', 
+				'player_name' => $playerData['fcUserName'],
+				'player_id'   => $playerData['fnUserID']
+			));
+		}
+
+		public function logout() {
+			$this->ci->session->set_userdata(array('logged_in' => ''));
+			$this->ci->session->sess_destroy();
+		}
+	}
