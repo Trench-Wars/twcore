069f87790e31700c21ab336d89430edf3d6b3c46
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-05-23 16:38:32 +0000

    Basic user authentication. Login/logout functions.
    git-svn-id: http://twcore.org/svn/trunk@6483 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twweb/application/controllers/home.php
+++ twweb/application/controllers/home.php
@@ -9,4 +9,28 @@
 			$this->load->view('home_index');
 			$this->load->view('include/footer');
 		}
+
+		public function login() {
+			if($this->input->get_post('player_name') != '' && $this->input->get_post('password') != '') {
+				$this->load->model('players');
+
+				// Lets check the login. If valid, set the session info
+				if($playerData = $this->players->checkPassword($this->input->get_post('player_name'), $this->input->get_post('password'))) {
+					$this->player_security->login($playerData);
+					redirect('');
+				} else {
+					$this->session->set_flashdata('message', 'INVALID LOGIN.');
+					redirect('home/login');
+				}
+			}
+
+			$this->load->view('include/header');
+			$this->load->view('home_login');
+			$this->load->view('include/footer');
+		}
+
+		public function logout() {
+			$this->player_security->logout();
+			redirect('');
+		}
 	}
--- twweb/application/views/include/header.php
+++ twweb/application/views/include/header.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  </head>
-  <body>
+  	<head>
+  	</head>
+  	<body>
+		<?if($this->player_security->isLoggedIn()) {?>
+			Welcome <?=$this->session->userdata('player_name');?>! (<a href="<?=base_url();?>home/logout" />logout</a>)
+		<?} else {?>
+			Welcome Guest! (<a href="<?=base_url();?>home/login">login</a>)
+		<?}?>
+		<br />
+
+		<?if($this->session->flashdata('message') != '') {?>
+			<span style="font-weight: bold;"><?=$this->session->flashdata('message');?></span>
+		<?}?>
+	
+  	
