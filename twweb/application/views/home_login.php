c5d99d29f20fda5471b97099f6f8218b0bbe4ee0
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-05-23 19:47:05 +0000

    basic login form
    git-svn-id: http://twcore.org/svn/trunk@6488 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twweb/application/views/home_login.php
@@ -0,0 +1,5 @@
+<form action="" method="POST" autocomplete="false">
+	<label for="player_name">Player Name:</label><input type="text" name="player_name" value="" /><br />
+	<label for="password">   Password:</label>   <input type="password" name="password" value="" /><br />
+	<input type="submit" value="Login" />
+</form>
