a26b4dca56242963da1eec37567d86e7163d24b4
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-05-23 19:34:57 +0000

    VERY basic html layout
    git-svn-id: http://twcore.org/svn/trunk@6487 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twweb/application/views/include/header.php
+++ twweb/application/views/include/header.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
   	<head>
+		<link rel="stylesheet" type="text/css" href="/css/main.css" />
   	</head>
   	<body>
-		<?if($this->player_security->isLoggedIn()) {?>
-			Welcome <?=$this->session->userdata('player_name');?>! (<a href="<?=base_url();?>home/logout" />logout</a>)
-		<?} else {?>
-			Welcome Guest! (<a href="<?=base_url();?>home/login">login</a>)
-		<?}?>
-		<br />
+		<div id="container">
+			<div id="header" style="width: 100%; padding-top: 10px;">
+				<div style="float:left; height: 50px;">
+					<img src="/img/tw_icon.png" />
+				</div>
+				<div style="float: right; height: 50px; color: #FFF;">
+			        <?if($this->player_security->isLoggedIn()) {?>
+		    	        Welcome <?=$this->session->userdata('player_name');?>! (<a href="<?=base_url();?>home/logout" />logout</a>)
+			        <?} else {?>
+			        	<a href="/home/login">Login!</a>
+					<?}?>	
+				</div>
+		  	</div>
+				
+			<div style="clear: both;"></div>
+
+			<div id="content">
+
 
 		<?if($this->session->flashdata('message') != '') {?>
 			<span style="font-weight: bold;"><?=$this->session->flashdata('message');?></span>
