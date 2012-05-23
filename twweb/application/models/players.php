1bc296eb8b9db3476e6a71b3871b0adf53d52d77
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-05-23 16:37:58 +0000

    Reworking of player model to support, but not require, a database schema change. Model code translates/joins tables and field names to unified schema.
    git-svn-id: http://twcore.org/svn/trunk@6482 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twweb/application/models/players.php
+++ twweb/application/models/players.php
@@ -1,4 +1,5 @@
-<?php	
+<?php
+	/* These functions should change the selected fields once the schema is updated. Then functions can be normalized.. o)-<| */
 	class Players extends MY_Model {
 		public $tableName = 'players';
 
@@ -6,9 +7,39 @@
 			parent::__construct();
 		}
 
+        public function findByName($playerId) {
+			// TODO: This should use Sphinx or something similar to translate text names to IDs, then
+			// the ID should be passed to the findById function.. 
+			$res = $this->db->query('SELECT fnUserID FROM tblUser WHERE fcUserName = ?', array($playerId));
+			if($row = $res->row_array()) return $this->findById($row['fnUserID']);
+			else return false;
+        }
+
+		public function findById($playerId) {
+            $res = $this->db->query('
+                SELECT 
+                    u.fnUserID          AS id,
+                    u.fcUserName        AS user_name,
+                    u.fdSignedUp        AS signup,
+                    u.fdDeleted         AS is_deleted,
+                    u.ftUpdated         AS last_update,
+                    ua.fcPassword       AS password,
+                    ua.fcIP             AS ip,
+                    ua.fdLastLoggedIn   AS last_login       
+                FROM tblUser u 
+                LEFT JOIN tblUserAccount ua ON ua.fnUserID=u.fnUserID 
+                WHERE u.fnUserID = ?', array($playerId));
+            return $res->row_array();
+        }
+
 		public function checkPassword($playerName, $password) {
-			$res = $this->db->query('SELECT * FROM tblUser u LEFT JOIN tblUserAccount ua ON ua.fnUserID=u.fnUserID WHERE ua.fcPassword = PASSWORD(?) AND u.fcUserName = ?', array($password, $playerName));
-			if($row = $res->row_array()) return $row;
+			$res = $this->db->query('
+				SELECT	u.fnUserID AS user_id
+				FROM tblUser u 
+				LEFT JOIN tblUserAccount ua ON ua.fnUserID=u.fnUserID 
+				WHERE ua.fcPassword = PASSWORD(?) AND u.fcUserName = ?', array($password, $playerName));
+
+			if($row = $res->row_array()) return $this->findById($row['user_id']);
 			else return false;
 		}
 	}
