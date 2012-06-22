2153a2bf21c26d9422c251da0b8e5f5b08aa0e0d
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-06-22 19:36:56 +0000

    design modifications
    git-svn-id: http://twcore.org/svn/trunk@6514 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twweb/application/views/include/header.php
+++ twweb/application/views/include/header.php
@@ -1,7 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
   	<head>
+		<script type="text/javascript" src="/js/ddtabmenu.js">
+		/***********************************************
+		* DD Tab Menu script- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
+		* This notice MUST stay intact for legal use
+		* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+		***********************************************/
+		</script>
+		
 		<link rel="stylesheet" type="text/css" href="/css/main.css" />
+
+		<script type="text/javascript">
+			ddtabmenu.definemenu("nav_menu", <?=isset($this->tab_num) ? $this->tab_num : -1;?>);
+		</script>
   	</head>
   	<body>
 		<div id="container">
@@ -9,7 +21,7 @@
 				<div style="float:left; height: 50px;">
 					<img src="/img/tw_icon.png" />
 				</div>
-				<div style="float: right; height: 50px; color: #FFF;">
+				<div style="float: right; height: 20px; color: #FFF; padding-top:30px;">
 			        <?if($this->player_security->isLoggedIn()) {?>
 		    	        Welcome <?=$this->session->userdata('player_name');?>! (<a href="<?=base_url();?>home/logout" />logout</a>)
 			        <?} else {?>
@@ -19,8 +31,19 @@
 		  	</div>
 				
 			<div style="clear: both;"></div>
+                <div id="nav_menu" class="chromemenu">
+                    <ul>
+                        <li><a href="<?=base_url();?>"      > Home    </a></li>
+                        <li><a href="<?=base_url();?>player"> Players </a></li>
+                        <li><a href="<?=base_url();?>squad" > Squads  </a></li>
+                    </ul>
+                </div>
 
 			<div id="content">
+
+				<div style="clear:both;"></div>
+
+				
 
 
 		<?if($this->session->flashdata('message') != '') {?>
