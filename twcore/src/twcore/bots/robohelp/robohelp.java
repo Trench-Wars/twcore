caa2ed542374224b4a3a1712b9c26dcc9a73857d
WingZero <WingZero@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-09-27 08:03:09 +0000

    testing auto responses
    git-svn-id: http://twcore.org/svn/trunk@6738 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/robohelp/robohelp.java
+++ twcore/src/twcore/bots/robohelp/robohelp.java
@@ -537,7 +537,9 @@ public class robohelp extends SubspaceBot {
             m_botAction.remotePrivateMessageSpam(playerName, helpRequest.getNextResponse());
             m_botAction.SQLBackgroundQuery(mySQLHost, "robohelp", "UPDATE tblCallHelp SET fcTakerName = '" + Tools.addSlashesToString("RoboHelp") + "', fnTaken = 1 WHERE fnCallID = "
                     + helpRequest.getCallID());
-
+            m_botAction.SQLBackgroundQuery(mySQLHost, "robohelp", "INSERT INTO tblCallAuto (fnCallID, fcPlayer, fcQuestion, fcResponse) VALUES(" +
+                    helpRequest.getCallID() + ", " + Tools.addSlashesToString(playerName) + 
+                    Tools.addSlashesToString(helpRequest.getMessage()) + ", " + Tools.addSlashesToString(helpRequest.getLastResponse()[0]) + ")");
             if (helpRequest.hasMoreResponses() == false) {
                 helpRequest.setAllowSummons(true);
                 m_botAction.sendRemotePrivateMessage(playerName, "If this is not helpful, type :" + m_botAction.getBotName() + ":!summon to request live help.");
