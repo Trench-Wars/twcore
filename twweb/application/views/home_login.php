a6fea08c29e8957643b535cfb59fd695c71ca261
o)-| <o)-|@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-10-06 03:22:18 +0000

    abandoned project
    git-svn-id: http://twcore.org/svn/trunk@6773 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twweb/application/views/home_login.php
+++
@@ -1,5 +0,0 @@
-<form action="" method="POST" autocomplete="false">
-	<label for="player_name">Player Name:</label><input type="text" name="player_name" value="" /><br />
-	<label for="password">   Password:</label>   <input type="password" name="password" value="" /><br />
-	<input type="submit" value="Login" />
-</form>
--- twweb/application/views/player_index.php
+++
@@ -1,3 +0,0 @@
-<form action="" method="POST">
-	Player Name: <input type="text" name="player_name" /> <input type="submit" value="Search!" />
-</form>
