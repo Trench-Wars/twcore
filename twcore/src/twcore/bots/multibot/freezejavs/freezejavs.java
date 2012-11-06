a3d2c4edc91880088c116456c04ec98c65a85ce1
k a n e <k a n e@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2012-11-06 08:41:22 +0000

    Added a team message to remind people in spec about !enter and !lagout. Fixed a sound issue when winners are announced. Changed the score list to 5 as per request by left_eye
    git-svn-id: http://twcore.org/svn/trunk@6956 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/multibot/freezejavs/freezejavs.java
+++ twcore/src/twcore/bots/multibot/freezejavs/freezejavs.java
@@ -444,10 +444,10 @@ public class freezejavs extends MultiModule {
     private void doStats(int winner) {
 
         m_botAction.sendArenaMessage("Kill = 1 pt /    Save = 1 pt /    Team Kills = -1 pt   ");
-        m_botAction.sendArenaMessage("-----------------------------------------------------TOP PLAYERS");
+        m_botAction.sendArenaMessage("-----------------------------------------------------TOP PLAYERS", 5);
         m_botAction.sendArenaMessage("   #        Kills        Saves        Score                   ");
 
-        for (int i = 1; i < 11; i++) { // Displays the top 10 players that were in the game                                       
+        for (int i = 1; i < 6; i++) { // Displays the top 10 players that were in the game                                       
 
             String name = getTopPlayer();
             PlayerInfo p = m_players.get(name);
@@ -465,7 +465,7 @@ public class freezejavs extends MultiModule {
             m_botAction.sendArenaMessage("---------The Warbirds have tagged their way to victory!---------");
             m_botAction.changeAllShips(WARBIRD);
         } else if (winner == JAVELIN) {
-            m_botAction.sendArenaMessage("---------The Javelins have tagged their way to victory!---------", 5);
+            m_botAction.sendArenaMessage("---------The Javelins have tagged their way to victory!---------");
             m_botAction.changeAllShips(JAVELIN);
         }
         isRunning = false;
@@ -515,6 +515,8 @@ public class freezejavs extends MultiModule {
                 doWarning();
             else if (timerInSeconds == 40)
                 doGo();
+            else if (timerInSeconds % 30 == 0)
+                m_botAction.sendTeamMessage("Please PM me with !enter to join the game or !lagout to return.");
             if (timerInSeconds == timeLimit && timeLimit > 0)
                 isTimer = true;
 
