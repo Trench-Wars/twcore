e08b5ee3bb73901e181a79c138c136e3096775e7
WingZero <WingZero@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2013-02-11 06:18:10 +0000

    git-svn-id: http://twcore.org/svn/trunk@7171 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/duel2bot/DuelPlayer.java
+++ twcore/src/twcore/bots/duel2bot/DuelPlayer.java
@@ -843,12 +843,13 @@ public class DuelPlayer {
     public void sql_createPlayer(String ip, String mid) {
         if (!create) return;
         create = false;
-        String a = "SELECT a2.fnUserID FROM tblDuel2__player P, tblAlias a1 JOIN tblAlias a2 ON a1.fnIP = a2.fnIP WHERE a1.fnUserID = " + userID + " AND p.fnUserID = a2.fnUserID AND p.fnEnabled = 1";
+        //String a = "SELECT a2.fnUserID FROM tblDuel2__player P, tblAlias a1 JOIN tblAlias a2 ON a1.fnIP = a2.fnIP WHERE a1.fnUserID = " + userID + " AND p.fnUserID = a2.fnUserID AND p.fnEnabled = 1";
         //String query = "SELECT fnUserID FROM tblDuel2__player P WHERE fnEnabled = 1 AND ((fcIP = '" + Tools.addSlashesToString(ip) + "' OR (fcIP = '" + ip + "' AND fnMID = " + mid + ")) OR fnUserID = " + userID + ")";
-        String query = "SELECT fnUserID FROM tblDuel2__player WHERE fnEnabled = 1 AND fnUserID IN (" + a + ")";
+        //String query = "SELECT fnUserID FROM tblDuel2__player WHERE fnEnabled = 1 AND fnUserID IN (" + a + ")";
+        String query = "SELECT DISTINCT DP.fnUserID FROM tblDuel2__player DP JOIN (tblAlias a1 JOIN tblAlias a2 ON a1.fnIP = a2.fnIP) ON a2.fnUserID = DP.fnUserID WHERE  a1.fnUserID = 7433 AND a1.fdUpdated > DATE_SUB(NOW(), INTERVAL 2 YEAR) AND a2.fdUpdated > DATE_SUB(NOW(), INTERVAL 2 YEAR) AND DP.fnEnabled = 1";
         ResultSet rs = null;
         try {
-            rs = ba.SQLQuery(db, a);
+            rs = ba.SQLQuery(db, query);
             if (staffer == null && rs.next()) {
                 String ids = "" + rs.getInt(1);
                 while (rs.next())
