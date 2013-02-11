6bab0fc0a0cbbf2e7fb7bac6e81f8c1e43d8cf3a
WingZero <WingZero@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2013-02-11 05:40:47 +0000

    git-svn-id: http://twcore.org/svn/trunk@7166 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/duel2bot/duel2bot.java
+++ twcore/src/twcore/bots/duel2bot/duel2bot.java
@@ -1339,7 +1339,7 @@ public class duel2bot extends SubspaceBot{
         if (alias.containsKey(name.toLowerCase())) {
             //String query = "SELECT u.fcUserName as n FROM tblDuel2__player p LEFT JOIN tblUser u ON p.fnUserID = u.fnUserID WHERE ";
             //query += "fnEnabled = 1 AND (fcIP = '" + ip + "' OR (fcIP = '" + ip + "' AND fnMID = " + mid + "))";    		
-            String query = "SELECT U.fcUserName FROM tblDuel2__player DP LEFT JOIN tblUser U ON DP.fnUserID = U.fnUserID WHERE DP.fnUserID IN (SELECT a2.fnUserID FROM tblAlias a1 JOIN tblAlias a2 ON a1.fnIP = a2.fnIP WHERE a1.fnUserID = (SELECT fnUserID FROM tblUser WHERE fcUserName = '"+Tools.addSlashesToString(name)+"' ORDER BY fnUserID ASC LIMIT 1)) AND fnEnabled = 1 LIMIT 100";
+            String query = "SELECT U.fcUserName as n FROM tblDuel2__player DP LEFT JOIN tblUser U ON DP.fnUserID = U.fnUserID WHERE DP.fnUserID IN (SELECT a2.fnUserID FROM tblAlias a1 JOIN tblAlias a2 ON a1.fnIP = a2.fnIP WHERE a1.fnUserID = (SELECT fnUserID FROM tblUser WHERE fcUserName = '"+Tools.addSlashesToString(name)+"' ORDER BY fnUserID ASC LIMIT 1)) AND fnEnabled = 1 LIMIT 100";
     		
             ba.SQLBackgroundQuery(db, "alias:" + alias.remove(name.toLowerCase()), query);
         }
