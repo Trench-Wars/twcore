2b350d9b032ee784135fb5b0cb0cb4814543f698
SpookedOne <SpookedOne@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2013-07-15 04:12:23 +0000

    http://twcore.org/ticket/582
    git-svn-id: http://twcore.org/svn/trunk@7436 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/multibot/acro2/acro2.java
+++ twcore/src/twcore/bots/multibot/acro2/acro2.java
@@ -66,7 +66,7 @@ public class acro2 extends MultiModule {
     }
     
     private void spamMessage(String message) {
-        spamMessage(message);
+        m_botAction.sendArenaMessage(message);
         spamChats(message);
     }
     
