5e7ca2229d97282c2811f8698b5ef3239308d10b
Trancid <Trancid@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2013-12-12 08:02:46 +0000

    Implemented tag-less bot access level assignment. This is backward compatible with the current staff structure. The assignment for the granted access is now as follows: - ZH access when staff powers are limited to certain arenas, denoted by :<arena name>, overrides ER and Mod level. - ER access when staff powers are limited to all arenas, denoted by :@, overrides Mod level. - Any other entries in moderate will be assigned Mod level. - Smod access to anyone in Smod. - Sysop access to anyone in Sysop. - Any special assignments in operators.cfg will override any of the above.
    git-svn-id: http://twcore.org/svn/trunk@8179 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/OperatorList.java
+++ twcore/src/twcore/core/OperatorList.java
@@ -60,6 +60,13 @@ public class OperatorList {
     public static final int SYSOP_LEVEL = 10;
     public static final int OWNER_LEVEL = 11;
     
+    private static final int MARK_NONE = 0;
+    private static final int MARK_TAG  = 1;
+    private static final int MARK_LINE = 2;
+    private static final int MARK_INCL = 3;
+    private static final int MARK_EXCL = 4;
+    
+    
 
     /**
      * This Hashmap contains all the operators
@@ -167,9 +174,6 @@ public class OperatorList {
     public void autoAssignFile(File data) {
         // 1. Cycle the autoAssign hashmap
         //
-
-        // Section dependent flag. Used for new rank Marketing staff. Has ZH access levels, but no tag.
-        boolean sectionMarketing = false;
         
         for(int level:autoAssign.keySet()) {
             String autoAssignSetting = autoAssign.get(level);
@@ -182,82 +186,139 @@ public class OperatorList {
             // If the auto assign setting starts with "moderate.txt" / "smod.txt" / "sysop.txt"
             if(autoAssignSetting.startsWith(data.getName()) ) {
                 // Load operators from this file into this level
-                String autoAssignSetting2 = null;
+                Integer autoAssignMarker = MARK_NONE;
+                String autoAssignParam1 = null;
+                String autoAssignParam2 = null;
                 if(autoAssignSetting.contains(":")) {
-                    autoAssignSetting2 = autoAssignSetting.substring(autoAssignSetting.indexOf(':'));
+                    String temp = autoAssignSetting.substring(autoAssignSetting.indexOf(':')).toLowerCase();
+                    if(temp == null || temp.isEmpty()) {
+                        autoAssignMarker = MARK_NONE;
+                    } else if(temp.startsWith(":tag ")) {
+                        autoAssignMarker = MARK_TAG;
+                        autoAssignParam1 = temp.substring(5);
+                    } else if(temp.startsWith(":line ")) {
+                        autoAssignMarker = MARK_LINE;
+                        String[] delimiters = temp.substring(6).split("\" - \""); // cut off :line " and split by " - "
+                        autoAssignParam1 = delimiters[0];
+                        autoAssignParam2 = delimiters[1];
+                    } else if(temp.startsWith(":incl ")) {
+                        autoAssignMarker = MARK_INCL;
+                        autoAssignParam1 = temp.substring(6);
+                    } else if(temp.startsWith(":excl ")) {
+                        autoAssignMarker = MARK_EXCL;
+                        autoAssignParam1 = temp.substring(6);
+                    }
                 }
 
                 // Read through the file and add operators
                 try {
                 	FileReader reader = new FileReader(data);
                     BufferedReader buffer = new BufferedReader(reader);
-                    String line = null, name = null;
+                    String line = null, name = null, arena = null;
                     boolean in_area = false;
 
                     while (( line = buffer.readLine()) != null) {
-
-                        if( line.startsWith("-")) {
-                            if(line.contains("TW Marketing/Player Retention Department:"))
-                                sectionMarketing = true;
-                            else if(line.contains("End note for Marketing staff - DO NOT REMOVE"))
-                                sectionMarketing = false;
-                        }
+                        
                         if( line.startsWith(" ") ||
                             line.startsWith("-") ||
-                            line.startsWith("+") ||
                             line.startsWith("/") ||
                             line.startsWith("*") ||
                             line.trim().length() == 0)
                             continue;
 
-                        name = line.trim().toLowerCase();
+                        if( line.startsWith("+") ) {
+                            // Skip lines starting with + if we aren't handling an :incl or :excl access level.
+                            if( autoAssignMarker != MARK_INCL && autoAssignMarker != MARK_EXCL )
+                                continue;
+
+                            // Otherwise, parse the name and arena.
+                            int index = line.indexOf(':');
+                            
+                            if(index == -1)
+                                continue;
+                            
+                            name = line.substring(1, index).trim().toLowerCase();
+                            arena = line.substring(index + 1).trim().toLowerCase(); 
+                        } else {
+                            // Skip lines not starting with + if we are handling an :incl or :excl access level.
+                            if ( autoAssignMarker == MARK_INCL || autoAssignMarker == MARK_EXCL)
+                                continue;
+                            
+                            // Otherwise, parse the name.
+                            name = line.trim().toLowerCase();
+                        }
 
                         // Ignore bot names that are already on the operator list as bot level
                         if(operators.containsKey(name) && operators.get(name) == OperatorList.BOT_LEVEL) {
                             continue;
                         }
-
-                        // if we're not using auto-assign options "tag" or "line"
-                        if(!sectionMarketing 
-                                && (autoAssignSetting2 == null || autoAssignSetting2.replace(":", "").length() == 0)) {
+                        
+                        switch(autoAssignMarker) {
+                        case MARK_NONE:
+                        default:
+                            // If the name already has a higher access level, continue, otherwise, add it to the appropiate level.
                             if(operators.containsKey(name) && operators.get(name) >= level) {
                                 continue;
                             } else {
                                 operators.put(name, level);
                             }
-                        }
-
-                        // If an operator is added below and he is already known in the operators map, he will
-                        // be overwritten. Because operators are read in from highest (owner) to lowest (ZH),
-                        // everyone will get the correct level
-                        if(autoAssignSetting2 != null && autoAssignSetting2.startsWith(":tag ") && autoAssignSetting2.length() > 6) {
-                            if(name.contains(autoAssignSetting2.toLowerCase().substring(5))) {
+                            break;
+                            
+                        case MARK_TAG:
+                            // If an operator is added below and he is already known in the operators map, he will
+                            // be overwritten. Because operators are read in from highest (owner) to lowest (ZH),
+                            // everyone will get the correct level
+                            if(autoAssignParam1 != null && name.contains(autoAssignParam1)) {
                                 operators.put(name, level);
                             }
-                        }
-                        if(sectionMarketing) {
-                            if(operators.containsKey(name) && operators.get(name) >= ZH_LEVEL) {
+                            break;
+                            
+                        case MARK_LINE:
+                            // Section based assignment.
+                            if(autoAssignParam1 == null || autoAssignParam2 == null)
                                 continue;
-                            } else {
-                                operators.put(name, ZH_LEVEL);
-                            }
-                        }
-                        if(autoAssignSetting2 != null && autoAssignSetting2.startsWith(":line")) {
-                            String[] delimiters = autoAssignSetting2.substring(7).split("\" - \""); // cut off :line " and split by " - "
-
-                            if(line.trim().toLowerCase().startsWith(delimiters[0].toLowerCase())) { // start area
+                            
+                            // Start of an area.
+                            if(line.trim().toLowerCase().startsWith(autoAssignParam1)) {
                                 in_area = true;
                                 continue;
                             }
-                            if(line.trim().toLowerCase().startsWith(delimiters[1].toLowerCase())) { // end area
+                            // End of an area.
+                            if(line.trim().toLowerCase().startsWith(autoAssignParam2)) {
                                 in_area = false;
                                 continue;
                             }
+                            
                             if(in_area) {
+                                // Override any previous notion.
                                 operators.put(name,level);
                             } else {
                                 continue;
                             }
+                            
+                            break;
+                            
+                        case MARK_INCL:
+                            // Only assign operators that have arenas assigned that include the parameter.
+                            if(arena != null && arena.contains(autoAssignParam1)) {
+                                // Override only when the player is at a moderator level, for now.
+                                if(operators.containsKey(name) && operators.get(name) != MODERATOR_LEVEL)
+                                    continue;
+                                
+                                operators.put(name, level);
+                            }
+                            break;
+                            
+                        case MARK_EXCL:
+                            // Only assign operators that have arenas assigned that exclude the parameter.
+                            if(arena != null && !arena.contains(autoAssignParam1)) {
+                                // Override only when the player is at a moderator level, for now.
+                                if(operators.containsKey(name) && operators.get(name) != MODERATOR_LEVEL)
+                                    continue;
+                                
+                                operators.put(name, level);
+                            }
+                            break;
                         }
                     }
                     
