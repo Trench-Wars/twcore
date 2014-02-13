5e7dd5c9b199220da36dae577934e15a6c91924e
qan <qan@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2014-02-13 19:40:36 +0000

    Much-needed comments
    git-svn-id: http://twcore.org/svn/trunk@8436 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/util/ipc/IPCCommand.java
+++ twcore/src/twcore/core/util/ipc/IPCCommand.java
@@ -1,35 +1,45 @@
 package twcore.core.util.ipc;
 
 /**
+ * Class used to send a specific command to another bot, with specific type enforced.
  *
  * @author WingZero
  */
 public class IPCCommand {
 
-    Command type;
-    String bot;
-    String command;
+    Command type;               // Type of command to send
+    String bot;                 // Name of bot this is being sent to. MAY BE NULL
+    String commandDescription;  // Description of the command being sent
     
     public IPCCommand(Command cmd, String msg) {
         type = cmd;
-        command = msg;
+        commandDescription = msg;
         bot = null;
     }
     
     public IPCCommand(Command cmd, String toBot, String msg) {
         type = cmd;
         bot = toBot;
-        command = msg;
+        commandDescription = msg;
     }
     
+    /**
+     * @return The actual Command associated with this IPCCommand.
+     */
     public Command getType() {
         return type;
     }
     
+    /**
+     * @return The String-based command sent -- usually just a description.
+     */
     public String getCommand() {
-        return command;
+        return commandDescription;
     }
     
+    /**
+     * @return The name of the bot this IPCCommand is intended for. CHECK FOR NULL
+     */
     public String getBot() {
         return bot;
     }
