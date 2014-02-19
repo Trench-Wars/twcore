8818628f27d6204838c9d412ffec0f8c33bec596
dezmond <dezmond@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2014-02-19 19:54:23 +0000

    Testing something and I don't want to shove this in any other bot.
    git-svn-id: http://twcore.org/svn/trunk@8486 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/serverbot/serverbot.java
@@ -0,0 +1,83 @@
+package twcore.bots.serverbot;
+
+import twcore.core.*;
+import twcore.core.events.*;
+import twcore.core.stats.DBPlayerData;
+import twcore.core.util.Tools;
+import twcore.core.events.SQLResultEvent;
+import twcore.core.game.Player;
+
+import java.net.*;
+import java.io.*;
+import java.sql.ResultSet;
+import java.util.*;
+
+public class serverbot extends SubspaceBot {
+    private BotAction BA;
+    private BotSettings BS;
+    private OperatorList oplist;
+    String pname;
+
+    public serverbot(BotAction botAction) {
+        super(botAction);
+
+        BA = BotAction.getBotAction();
+        BS = BA.getBotSettings();
+        EventRequester req = BA.getEventRequester();
+        oplist = BA.getOperatorList();
+
+        req.request(EventRequester.MESSAGE);
+        req.request(EventRequester.ARENA_JOINED);
+        req.request(EventRequester.LOGGED_ON);
+        pname = null;
+    }
+
+    //Standard Message Event for TWCore. Commands and stuff!
+    public void handleEvent(Message event) {
+        String msg = event.getMessage();
+        int msgtype = event.getMessageType();
+        String name = event.getMessager();
+        if (name == null)
+            name = BA.getPlayerName(event.getPlayerID());
+
+        if (pname == null)
+            pname = name;
+
+        if ((msgtype == Message.PRIVATE_MESSAGE) || (msgtype == Message.REMOTE_PRIVATE_MESSAGE)) {
+        }
+    }
+
+
+    public void handleEvent(LoggedOn event) {
+        BA.joinArena(BS.getString("arena"));
+    }
+
+    public void handleEvent(ArenaJoined event) {
+        BA.setReliableKills(1);
+    }
+
+    public void handleEvent(SQLResultEvent event) {
+    }
+
+    public void handleEvent(InterProcessEvent event) {
+      
+    }
+
+    public void handleEvent(SocketMessageEvent event){
+
+    	if (event.getRequest().equals("GETPLAYERS")) {
+    		ba.sendSmartPrivateMessage("Dezmond", "Connected successfully.");
+    		Iterator<Player> it = m_botAction.getPlayerIterator();
+    		
+    		// Building a JSON-format result
+    		StringBuilder builder = new StringBuilder();
+    		while(it.hasNext()) {
+    			Player p = it.next();
+    			builder.append(p.getPlayerName()+":"+p.getXTileLocation()+":"+p.getYTileLocation()+":"+p.getShipType() + "$$:$$");
+        		ba.sendSmartPrivateMessage("Dezmond", "Successfully sent to script...");
+    		}
+
+    		event.setResponse(builder.toString());
+    	}
+    }
+}
