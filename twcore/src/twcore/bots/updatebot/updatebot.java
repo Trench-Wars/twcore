8aa7e160371951943e2638faebbd8ced66879a91
CRe <CRe@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2014-03-12 15:31:40 +0000

    New changes.
    git-svn-id: http://twcore.org/svn/trunk@8607 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/updatebot/updatebot.java
+++ twcore/src/twcore/bots/updatebot/updatebot.java
@@ -13,6 +13,7 @@ import twcore.core.SubspaceBot;
 import twcore.core.events.ArenaJoined;
 import twcore.core.events.LoggedOn;
 import twcore.core.events.Message;
+import twcore.core.util.Tools;
 
 
 /**
@@ -29,7 +30,13 @@ public class updatebot extends SubspaceBot {
     private Vector<String> commandsToExecute;
     
     private boolean ready;
+    private boolean completedTask;
     private String coreLocation;
+    private String executor;
+    
+    private final int ARENA_SWITCH_DELAY = 30000;
+    
+    private ArenaSwitch arenaSwitch;
     
     public updatebot(BotAction botAction) {
         super(botAction);
@@ -42,25 +49,24 @@ public class updatebot extends SubspaceBot {
         arenas = new Vector<String>();
         
         ready = false;
+        completedTask = false;
         
-        retrieveArenaList();
+        retrieveArenaList();        
         
+        arenaSwitch = new ArenaSwitch();
     }
 
-    private void retrieveArenaList()
-    {
-    	try (BufferedReader br = new BufferedReader(new FileReader(coreLocation + "/twcore/bots/updatebot/arenas.txt")))
-    	{
- 
-			String currentLine;
+    private void retrieveArenaList() {
+    	try (BufferedReader br = new BufferedReader(new FileReader(coreLocation + "/twcore/bots/updatebot/arenas.txt"))) { 
+			
+    		String currentLine;
  
 			while ((currentLine = br.readLine()) != null) {
 				arenas.add(currentLine);
-				System.out.println("Added " + currentLine);
-			}
- 
+				Tools.printLog("UpdateBot: Added " + currentLine);
+			} 
 		} catch (IOException e) {
-			e.printStackTrace();
+			Tools.printStackTrace(e);
 		} 
     }
 
@@ -85,24 +91,25 @@ public class updatebot extends SubspaceBot {
         if (name == null) {
         	name = m_botAction.getPlayerName(event.getPlayerID());
         }
+        
+        if (name == null) return;
     	
     	String[] helpmsg = {
     			"!help - Derp",
     			"!addcommand <command> - Adds command to command list",
     			"!removecommand <#> - Removes command from command list. Find # in !commandlist.",
     			"!listcommands - Lists added commands.",
-    			"!cycle - Cycles through every arena and executes command(s).",
+    			"!cycle - Cycles through every arena and executes command(s). Bot will notify & shutdown when done.",
     			"!die - Murders this bot.",
     			"This bot cycles through arenas in a provided file and executes the inputed commands."};
     	
-        if(m_botAction.getOperatorList().isSysop(name)) {
-        	
+        if(m_botAction.getOperatorList().isSysop(name) && !ready) {        	
         	if(msg.startsWith("!help")) {
         		m_botAction.privateMessageSpam(name, helpmsg);
-        	} else if (msg.startsWith("!addcommand")) {
+        	} else if (msg.startsWith("!addcommand ")) {
         		do_addCommand(name, msg.substring(12));
-        	} else if (msg.startsWith("!removecommand")) {
-        		do_removeCommand(name, msg.substring(15).trim());
+        	} else if (msg.startsWith("!removecommand ")) {
+        		do_removeCommand(name, msg.substring(15));
         	} else if (msg.startsWith("!listcommands")) {
         		do_listCommand(name);
         	} else if (msg.startsWith("!cycle")) {
@@ -130,11 +137,13 @@ public class updatebot extends SubspaceBot {
     		m_botAction.sendPrivateMessage(name, "Error: Invalid input.");
     	}
     	
-    	if(commandsToExecute.size() > 0)
+    	if(commandsToExecute.size() > 0 && !(i > commandsToExecute.size()))
     	{
     		commandsToExecute.remove(i);
     		m_botAction.sendPrivateMessage(name, "Removed command.");
     	}
+    	else
+    		m_botAction.sendPrivateMessage(name, "Error: Invalid input.");
     }
     
     private void do_listCommand(String name) {
@@ -147,53 +156,47 @@ public class updatebot extends SubspaceBot {
     }
 
     private void do_cycle(String name)   {
-    	ArenaSwitch as = new ArenaSwitch(name);
-    	m_botAction.scheduleTask(as, 1000, 30000);
+    	m_botAction.scheduleTask(arenaSwitch, 1000, ARENA_SWITCH_DELAY);
     	ready = true;
-    	m_botAction.sendPrivateMessage(name, "Cycle has begun.");
-    	m_botAction.sendPrivateMessage(name, "Estimated time for completion is about: " + 30*arenas.size() + " seconds.");
-    	
+    	executor = name;
+    	m_botAction.sendPrivateMessage(name, executor);
+    	m_botAction.sendPrivateMessage(name, "Cycle has begun. Will ignore all commands now.");
+    	m_botAction.sendPrivateMessage(name, "Estimated time for completion is about: " + (((double)ARENA_SWITCH_DELAY+1000.0)/60000.0)*(double)arenas.size() + " minutes.");
+    	Tools.printLog("UpdateBot: Cycle started by "+name);
     }
-
-
 
     public void handleEvent(LoggedOn event) {
         m_botAction.joinArena(m_botSettings.getString("arena"));
     }
 
     public void handleEvent(ArenaJoined event) {
-    	if(ready)
-    	{
+    	if(ready) {
     		for(int i = 0; i < commandsToExecute.size(); i++)
-    		{
     			m_botAction.sendUnfilteredPublicMessage(commandsToExecute.get(i));
-    		}
     	}
-
+    }
+    
+    public void handleDisconnect() {
+    	arenaSwitch.cancel();
+    	String status = completedTask ? "completed. " : "not completed. ";
+    	Tools.printLog("UpdateBot: Shutting down. Cycle " + status + "Arena: "
+    		+ m_botAction.getArenaName());
     }
     
     private class ArenaSwitch extends TimerTask
-    {
-    	private String executor;
-    	
-    	public ArenaSwitch(String executor) {
-    		this.executor = executor;
-    	}
-    	
-    	
-    	public void run()
-    	{
+    {    	
+    	public ArenaSwitch() {}
+    	    	
+    	public void run() {
     		if (arenas.size() > 0) {
-    			m_botAction.joinArena(arenas.remove(0));
+    			m_botAction.changeArena(arenas.remove(0));
     		}
-    		else
-    		{
-    			m_botAction.sendRemotePrivateMessage(executor, "Task completed.");
-    			this.cancel();
-    			
-    		}
-    		
+    		else {
+    			m_botAction.sendRemotePrivateMessage(executor, "Task completed. Shutting down.");
+    			ready = false;
+    			completedTask = true;
+    			m_botAction.die();
+    		}    		
     	}
     }
-
 }
