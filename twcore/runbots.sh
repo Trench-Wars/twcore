26465e2616750853ab2506aaf13244a97ee8f7f1
24 <24@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2015-12-24 18:33:26 +0000

    jar testing
    git-svn-id: http://twcore.org/svn/trunk@9082 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/runbots.sh
+++ twcore/runbots.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do java -Xmx386m -cp bin/twcore.jar:libs/googleapi.jar:libs/aim.jar:lib/mysql-connector-java-5.0.7-bin.jar:lib/gson.jar twcore.core.Start bin/setup.cfg; done
+while true; do java -Xmx386m -cp bin/twcore.jar:libs/googleapi.jar:libs/tyrus-standalone-client-1.8.3.jar:libs/aim.jar:lib/mysql-connector-java-5.0.7-bin.jar:lib/gson.jar twcore.core.Start bin/setup.cfg; done
