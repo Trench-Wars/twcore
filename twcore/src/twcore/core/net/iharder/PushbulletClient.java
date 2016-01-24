3d71d4fe3a288d49e09dbc3989f269a8ee4c90d2
24 <24@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2016-01-24 05:32:36 +0000

    updates to queries and verify
    git-svn-id: http://twcore.org/svn/trunk@9229 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/core/net/iharder/PushbulletClient.java
+++ twcore/src/twcore/core/net/iharder/PushbulletClient.java
@@ -270,6 +270,7 @@ public class PushbulletClient{
      */
     private void handleOnWebSocketMessage( String msg ){
 //        LOGGER.info("1");
+    	  LOGGER.info("test");
         
     	StreamMessage smsg = null;
         try {
