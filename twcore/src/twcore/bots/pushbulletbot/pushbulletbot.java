e65c67a8cf0cc9c4210e854fcbac78973889751b
24 <24@9f8b3893-2a26-0410-9d00-a71e40fd2f77>
2016-01-24 04:46:56 +0000

    syncronizing twdhub pushbullet functions with the newest pushbullet updates
    git-svn-id: http://twcore.org/svn/trunk@9227 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pushbulletbot/pushbulletbot.java
+++ twcore/src/twcore/bots/pushbulletbot/pushbulletbot.java
@@ -321,8 +321,7 @@ public class pushbulletbot extends SubspaceBot {
         switch (statementName.toLowerCase()) {
         case "signup":
             preparedStatement =
-                "USE trench_TrenchWars;"
-                +   "SET @PlayerName = ?, @PushBulletEmail = ?;"
+                   "SET @PlayerName = ?, @PushBulletEmail = ?;"
                 +   "DELETE PBA FROM trench_TrenchWars.tblPBAccount AS PBA WHERE fbVerified = 0 AND TIMESTAMPDIFF(MINUTE, fdCreated ,NOW()) > 30;"
                 +   "DELETE PBA FROM trench_TrenchWars.tblPBAccount AS PBA "
                 +   "JOIN trench_TrenchWars.tblUser AS U ON U.fnUserID = PBA.fnPlayerID AND U.fcUserName = @PlayerName;"
